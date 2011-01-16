20efcf3cd35cc57a393fcefe2228a8face44a3fc
Hyeontaek Lim <hl@cs.cmu.edu>
2011-01-15 23:19:12 -0500

    updated packet forwarding experiment scripts

---
+++ click/conf/microbench/run_packetforward.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+make -C ../.. || exit 1
+
+sudo opcontrol --no-vmlinux || exit 1
+
+function run {
+	sudo opcontrol --reset || exit 1
+	sudo opcontrol --start || exit 1
+	/usr/bin/time ./click $1.click >& output_$1_timing || exit 1
+	sudo opcontrol --shutdown || exit 1
+	opreport --demangle=smart --symbols click >& output_$1_oprof || exit 1
+}
+
+run ip_packetforward
+run xia_packetforward
+run xia_packetforward_fallback1
+run xia_packetforward_fallback2
+run xia_packetforward_update
+
