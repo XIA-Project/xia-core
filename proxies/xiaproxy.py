765f7a49d4382bdc138b9b054275ca5dfd8b429c
Boyan <boyanl@cs.cmu.edu>
2011-01-18 01:35:44 -0500

    Modified for experiments.

--- proxies/xiaproxy.py
+++ proxies/xiaproxy.py
@@ -1,5 +1,5 @@
 import socket, select, xia_pb2
-import struct
+import struct, time
 
 #CID = ['0000000000000000000000000000000000000000', '0000000000000000000000000000000000000001', '0000000000000000000000000000000000000010','0000000000000000000000000000000000000011','0000000000000000000000000000000000000100', '0000000000000000000000000000000000000101', '0000000000000000000000000000000000000110','0000000000000000000000000000000000000111','0000000000000000000000000000000000001000', '0000000000000000000000000000000000001001', '0000000000000000000000000000000000001010', '0000000000000000000000000000000000001011']
 
@@ -28,9 +28,11 @@ def read_write(bsoc, soc_rpc, max_idling=20):
                     if data:
 			    if (i == soc_rpc): #and (msg_response.appid == bsoc.fileno())):
 				    print "payload len (recv): %d" % len(msg_response.payload)
-				    print "payload (recv): %s" % msg_response.payload
+				    #print "payload (recv): %s" % msg_response.payload
 				    print len(msg_response.payload)
-				    payload_http = 'HTTP/1.1 200 OK\nDate: Sat, 08 Jan 2011 22:25:07 GMT\nServer: Apache/2.2.17 (Unix)\nLast-Modified: Sat, 08 Jan 2011 21:08:31 GMT\nETag: "43127-76-4995c231bf691"\nAccept-Ranges: bytes\nContent-Length: ' + str(len(msg_response.payload)) +  '\nConnection: close\nContent-Type: text/html\n\n' + msg_response.payload + '\r\n\r\n'
+				    payload_http = 'HTTP/1.1 200 OK\nDate: Sat, 08 Jan 2011 22:25:07 GMT\nServer: Apache/2.2.17 (Unix)\nLast-Modified: Sat, 08 Jan 2011 21:08:31 GMT\nCache-Control: no-cache\nAccept-Ranges: bytes\nContent-Length: ' + str(len(msg_response.payload)) +  '\nConnection: close\nContent-Type: text/html\n\n' + msg_response.payload + '\r\n\r\n'
+				    print "SID response"
+				    print time.time()
 				    out.send(payload_http)  
 			    count = 0
             else:
@@ -43,7 +45,7 @@ def sendSIDRequest(netloc, payload, bsoc, sock_rpc):
 	msg = xia_pb2.msg()
 	#msg.xid = '00000000000000000000'
 	msg.type = xia_pb2.msg.CONNECTSID
-	payload = ''     # to be removed
+	#payload = ''     # to be removed
         msg.payload = payload
         msg.xiapath_src = 'AD:1000000000000000000000000000000000000000 HID:0000000000000000000000000000000000000000'
         msg.xiapath_dst = 'AD:1000000000000000000000000000000000000001 HID:0000000000000000000000000000000000000001'  # sp SID: xxx
@@ -61,6 +63,7 @@ def sendSIDRequest(netloc, payload, bsoc, sock_rpc):
 def getCID(CID, sock_rpc):
 	print "in getCID function"  
 	print CID
+	print time.time()
 	msg = xia_pb2.msg()
 	msg.type = xia_pb2.msg.GETCID
         msg.payload = ''
@@ -78,14 +81,13 @@ def getCID(CID, sock_rpc):
 	size = struct.unpack('!i', data)[0]
 	data = sock_rpc.recv(size)
 
-	#data2 = sock_rpc.recv(4)
-	#size2 = struct.unpack('!i', data2)[0]
-	#data3 = sock_rpc.recv(size2)
+
 
 	msg_response = xia_pb2.msg()
 	msg_response.ParseFromString(data) 
 	payload =  msg_response.payload
 	print 'here here here:' + str (len(payload))
+	print time.time()
 	###print payload
 	
 	return payload
@@ -94,14 +96,18 @@ def getCID(CID, sock_rpc):
 def xiaHandler(control, payload, bsock, sock_rpc):
 	if payload.find('GET /favicon.ico') != -1:
 			return
-	print "in XIA code\n" + control + "\n" + payload
+	#print "in XIA code\n" + control + "\n" + payload
 	control=control[4:]
 	if control.find('sid') == 0:
 		print "SID request"
+		print time.time()
+		if control.find('image.jpg') != -1:
+			payload = 'image.jpg'
 		sendSIDRequest(control[4:], payload, bsock, sock_rpc);
 	elif control.find('cid') == 0:
 		print "cid request"
 		num = int(control[4])
+		#num = 1
 		print "num %d" % num
 		
 		payload_cid = ''
@@ -112,8 +118,8 @@ def xiaHandler(control, payload, bsock, sock_rpc):
 		length = len (payload_cid)
 		print length
 		#header = 'HTTP/1.1 200 OK\nETag: "48917-39ed-4990ddae564c"\nAccept-Ranges: bytes\nContent-Length: ' + str(length) + '\nContent-Type: image/jpeg\n\n'  # todo: avoid hard code 
-		header2 = 'HTTP/1.1 200 OK\nETag: "48917-39ed-4990ddae564c"\nAccept-Ranges: bytes\nContent-Length: 14829\nContent-Type: image/jpeg\n\n'  # todo: avoid hard code 
-		header = ''
+		header = 'HTTP/1.1 200 OK\nAccept-Ranges: bytes\nCache-Control: no-cache\nContent-Length: 14829\nContent-Type: image/jpeg\n\n'  # todo: avoid hard code 
+		header2 = ''
 		#getCID(control[4:], payload, bsock, sock_rpc);
 		payload_http = header + payload_cid
 		bsock.send(payload_http)
