09671f89b945df4d5c52144cd76d0584210fd27a
Boyan <boyanl@cs.cmu.edu>
2011-01-29 00:59:59 -0500

    Support no fallback scenario by timeout.

--- proxies/xiaproxy.py
+++ proxies/xiaproxy.py
@@ -1,9 +1,33 @@
 import socket, select, xia_pb2
-import struct, time
+import struct, time, signal, os
 
 #CID = ['0000000000000000000000000000000000000000', '0000000000000000000000000000000000000001', '0000000000000000000000000000000000000010','0000000000000000000000000000000000000011','0000000000000000000000000000000000000100', '0000000000000000000000000000000000000101', '0000000000000000000000000000000000000110','0000000000000000000000000000000000000111','0000000000000000000000000000000000001000', '0000000000000000000000000000000000001001', '0000000000000000000000000000000000001010', '0000000000000000000000000000000000001011']
-timestamps = [0, 0, 0, 0, 0, 0]
+timestamps = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 stamp_i = 0
+
+
+def timeout(CID, sock_rpc,  timeout_duration):
+    ready = select.select([sock_rpc], [], [], timeout_duration)
+    if ready[0]:
+        data = sock_rpc.recv(4)
+        size = struct.unpack('!i', data)[0]
+	print "receive picture size", size
+	data = sock_rpc.recv(size)
+        msg_response = xia_pb2.msg()
+	msg_response.ParseFromString(data) 
+	payload =  msg_response.payload
+	print 'here here here:' + str (len(payload))
+	print "%.6f" % time.time()
+	global stamp_i
+	timestamps[stamp_i] = time.time()
+	stamp_i = stamp_i + 1 
+    else :
+        print 'timeout:'
+        #sock_rpc.send('timeout')
+        payload = getCID (CID, sock_rpc, True)
+
+    return payload
+
 
 def read_write(bsoc, soc_rpc, max_idling=20):
         iw = [bsoc, soc_rpc]
@@ -36,13 +60,14 @@ def read_write(bsoc, soc_rpc, max_idling=20):
 				    global stamp_i
 				    timestamps[stamp_i] = time.time()
 				    stamp_i = stamp_i + 1 
-				    rt = msg_response.payload.find('CID')
+				    # distinguish modes 1 and 2
+				    rt = msg_response.payload.find('CID') 
 				    print rt
 				    if (rt!= -1):
 					    http_header = SIDResponse[0:rt]
-					    print '!!'+ http_header
-					    print SIDResponse[rt+4:rt+44]
-					    payload = getCID (SIDResponse[rt+4:rt+44], soc_rpc)
+					    #print '!!'+ http_header
+					    #print SIDResponse[rt+4:rt+44]
+					    payload = getCID (SIDResponse[rt+4:rt+44], soc_rpc, False)
 					    out.send (http_header+payload)
 				    else:
 					    out.send(SIDResponse)  
@@ -72,7 +97,7 @@ def sendSIDRequest(netloc, payload, bsoc, sock_rpc):
 	return
 
 
-def getCID(CID, sock_rpc):
+def getCID(CID, sock_rpc, fallback):
 	print "in getCID function"  
 	print CID
 	print "%.6f" % time.time()
@@ -83,7 +108,10 @@ def getCID(CID, sock_rpc):
 	msg.type = xia_pb2.msg.GETCID
         msg.payload = ''
         msg.xiapath_src = 'AD:1000000000000000000000000000000000000000 HID:0000000000000000000000000000000000000000'
-        msg.xiapath_dst = '( AD:1000000000000000000000000000000000000001 HID:0000000000000000000000000000000000000001 ) CID:'+ CID
+	if fallback == True :
+		msg.xiapath_dst = '( AD:1000000000000000000000000000000000000001 HID:0000000000000000000000000000000000000001 ) CID:'+ CID
+	else :
+		msg.xiapath_dst = 'CID:'+ CID
         serialized_msg = msg.SerializeToString()
 	#print ''.join(map(lambda x: '%02x' % ord(x), serialized_msg))
         size = struct.pack('!i', len(serialized_msg))
@@ -92,22 +120,15 @@ def getCID(CID, sock_rpc):
 
         sock_rpc.send(size)
         sock_rpc.send(serialized_msg)
-	data = sock_rpc.recv(4)
-	size = struct.unpack('!i', data)[0]
-	print "receive picture size", size
-	data = sock_rpc.recv(size)
+	#data = sock_rpc.recv(4)
+	#size = struct.unpack('!i', data)[0]
+	#print "receive picture size", size
+	#data = sock_rpc.recv(size)
+	data = timeout(CID, sock_rpc, 0.12)
 
 
-
-	msg_response = xia_pb2.msg()
-	msg_response.ParseFromString(data) 
-	payload =  msg_response.payload
-	print 'here here here:' + str (len(payload))
-	print "%.6f" % time.time()
-	global stamp_i
-	timestamps[stamp_i] = time.time()
-	stamp_i = stamp_i + 1 
-	return payload
+	
+	return data
 
 
 def xiaHandler(control, payload, bsock, sock_rpc):
@@ -132,7 +153,7 @@ def xiaHandler(control, payload, bsock, sock_rpc):
 		payload_cid = ''
 		for i in range (0, num):
 			print i
-			payload_cid += getCID(control[6+i*40+i:46+i*40+i], sock_rpc)
+			payload_cid += getCID(control[6+i*40+i:46+i*40+i], sock_rpc, False)
 			
 		length = len (payload_cid)
 		print length
