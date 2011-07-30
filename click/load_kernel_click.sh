02b97d26a1675c8bf3ba2fadf4921766d8d94aa2
Hyeontaek Lim <hl@cs.cmu.edu>
2011-07-30 19:27:11 -0400

    made load script less verbose

--- click/load_kernel_click.sh
+++ click/load_kernel_click.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-make -C `dirname $0`/ -j || exit 1
+make -s -C `dirname $0`/ -j || exit 1
 
 ################
 
-sudo rmmod click
-sudo rmmod proclikefs
+sudo rmmod click 2> /dev/null
+sudo rmmod proclikefs 2> /dev/null
 
-#sudo make -C `dirname $0`/linuxmodule/ uninstall || exit 1
-#sudo make -C `dirname $0`/tools/click-install/ uninstall || exit 1
+#sudo make -s -C `dirname $0`/linuxmodule/ uninstall || exit 1
+#sudo make -s -C `dirname $0`/tools/click-install/ uninstall || exit 1
 
-sudo ifdown eth2 &
-sudo ifdown eth3 &
-sudo ifdown eth4 &
-sudo ifdown eth5 &
+sudo ifdown eth2 2> /dev/null &
+sudo ifdown eth3 2> /dev/null &
+sudo ifdown eth4 2> /dev/null &
+sudo ifdown eth5 2> /dev/null &
 wait
 
 sudo rmmod ixgbe
@@ -22,14 +22,14 @@ sudo rmmod ixgbe
 
 sudo modprobe ixgbe RSS=4,4,4,4
 
-sudo ifup eth2 &
-sudo ifup eth3 &
-sudo ifup eth4 &
-sudo ifup eth5 &
+sudo ifup eth2 2> /dev/null &
+sudo ifup eth3 2> /dev/null  &
+sudo ifup eth4 2> /dev/null &
+sudo ifup eth5 2> /dev/null  &
 wait
 
-sudo make -C `dirname $0`/linuxmodule/ install || exit 1
-sudo make -C `dirname $0`/tools/click-install/ install || exit 1
+sudo make -s -C `dirname $0`/linuxmodule/ install || exit 1
+sudo make -s -C `dirname $0`/tools/click-install/ install || exit 1
 
 ################
 
