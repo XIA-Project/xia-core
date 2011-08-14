73b4e2bd15afa681b473170a3c4f3e020e781ca5
Hyeontaek Lim <hl@cs.cmu.edu>
2011-08-14 03:48:48 -0400

    clean up

--- click-2.0/load_kernel_click.sh
+++ click-2.0/load_kernel_click.sh
@@ -4,8 +4,9 @@ make -s -C `dirname $0`/ -j || exit 1
 
 ################
 
-sudo rmmod click 2> /dev/null
-sudo rmmod proclikefs 2> /dev/null
+sudo click-uninstall
+#sudo rmmod click 2> /dev/null
+#sudo rmmod proclikefs 2> /dev/null
 
 #sudo make -s -C `dirname $0`/linuxmodule/ uninstall || exit 1
 #sudo make -s -C `dirname $0`/tools/click-install/ uninstall || exit 1
@@ -21,7 +22,6 @@ sudo rmmod ixgbe
 ################
 
 sudo modprobe ixgbe RSS=6,6,6,6
-
 
 #sudo ethtool -A eth2 autoneg off
 #sudo ethtool -A eth2 tx off
@@ -50,9 +50,9 @@ sudo modprobe ixgbe RSS=6,6,6,6
 #sudo ethtool -A eth5 rx off
 
 sudo ifup eth2 2> /dev/null &
-sudo ifup eth3 2> /dev/null  &
+sudo ifup eth3 2> /dev/null &
 sudo ifup eth4 2> /dev/null &
-sudo ifup eth5 2> /dev/null  &
+sudo ifup eth5 2> /dev/null &
 wait
 
 sudo make -s -C `dirname $0`/linuxmodule/ install-local || exit 1
