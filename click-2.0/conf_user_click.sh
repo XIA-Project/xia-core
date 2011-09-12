9d27fd1bb6db8b0888c224a3a4167cf7fdfb7a3f
Hyeontaek Lim <hl@cs.cmu.edu>
2011-09-12 02:19:39 -0400

    enabled userlevel multithreading

--- click-2.0/conf_user_click.sh
+++ click-2.0/conf_user_click.sh
@@ -5,5 +5,5 @@ IO_ENGINE=`readlink -f $IO_ENGINE`
 
 cd `dirname $0`/
 #./configure --disable-linuxmodule --enable-warp9 --enable-multithread=24 --enable-userlevel --enable-dmalloc  # for debugging 
-CPPFLAGS="-I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-multithread=24 --enable-userlevel
+CPPFLAGS="-I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel
 
