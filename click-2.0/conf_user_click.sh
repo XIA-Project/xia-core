f8c61f83e11031f9fd2e653df8f62a72d89ec7cc
Hyeontaek Lim <hl@cs.cmu.edu>
2011-09-20 21:18:54 -0400

    added non-multithreading configuration for click

--- click-2.0/conf_user_click.sh
+++ click-2.0/conf_user_click.sh
@@ -4,6 +4,5 @@ IO_ENGINE=`dirname $0`/../io_engine
 IO_ENGINE=`readlink -f $IO_ENGINE`
 
 cd `dirname $0`/
-#./configure --disable-linuxmodule --enable-warp9--enable-userlevel --enable-dmalloc  # for debugging 
-CPPFLAGS="-I$IO_ENGINE/include -D_GNU_SOURCE" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel --enable-multithread=24  --enable-ip6 --enable-ipsec
+CPPFLAGS="-I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel --enable-multithread=24  --enable-ip6 --enable-ipsec
 
