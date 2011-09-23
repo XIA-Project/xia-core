e94ae6c3905d3927edd9209c19a99a502b14508a
Hyeontaek Lim <hl@cs.cmu.edu>
2011-09-23 18:14:54 -0400

    updated migration timeline graph

--- migration/plot.gnuplot
+++ migration/plot.gnuplot
@@ -1,23 +1,34 @@
 #!/usr/bin/env gnuplot
 
-set terminal pdf enhanced
+set terminal pdf enhanced dashed size 7,3.5
 set output "plot.pdf"
 
-#set y2tics ("HID_A" 1, "HID_B" 2)
-#set nomy2tics
+set ytics nomirror
 
-set xlabel "Elapsed time (seconds)"
+set y2tics autofreq
+set my2tics
+
+set xlabel "Elapsed time (milliseconds)"
 set ylabel "Transfer rate (pps)"
+set y2label "Sequence number (K)"
 
-set key left bottom
+set key box left bottom
 
-plot "plot.dat" index 0 using 1:($2) axes x1y1 with lines linewidth 4 notitle, \
-     ""		index 1 using 1:($2) axes x1y2 with points ls 19 ps 0.1 lt 3 notitle, \
-     ""         index 2 using 1:($2) axes x1y2 with points ls 19 ps 0.1 lt 3 notitle, \
-     ""         index 5 using 1:($2) axes x1y2 with points ls 19 ps 0.1 lt 4 notitle, \
-     ""         index 6 using 1:($2) axes x1y2 with points ls 19 ps 0.1 lt 4 notitle, \
-     ""		index 3 using 1:($3 + 1) axes x1y2 with points ls 19 ps 0.1 lt 3 notitle, \
-     ""         index 4 using 1:($3 + 1) axes x1y2 with points ls 19 ps 0.1 lt 3 notitle, \
-     ""         index 7 using 1:($3 + 1) axes x1y2 with points ls 19 ps 0.1 lt 4 notitle, \
-     ""         index 8 using 1:($3 + 1) axes x1y2 with points ls 19 ps 0.1 lt 4 notitle
+set xrange [0:2000]
+set yrange [0:1100]
+set y2range [0:40]
 
+plot "plot.dat" \
+                index 7 using ($1 * 1000 - 11000):($3 / 1000 + 1 + 10) axes x1y2 with points ls 7 ps 0.3 lt 3 title "Server reception", \
+     ""         index 8 using ($1 * 1000 - 11000):($3 / 1000 + 1 + 10) axes x1y2 with points ls 7 ps 0.3 lt 3 notitle, \
+     ""		index 3 using ($1 * 1000 - 11000):($3 / 1000 + 0 + 10) axes x1y2 with points ls 7 ps 0.3 lt 4 title "Client reception", \
+     ""         index 4 using ($1 * 1000 - 11000):($3 / 1000 + 0 + 10) axes x1y2 with points ls 7 ps 0.3 lt 4 notitle, \
+     ""		index 0 using ($1 * 1000 - 11000):($2) axes x1y1 with lines linewidth 8 lt 1 title "Client reception rate ", \
+     ""		index 9 using ($1 * 1000 - 11000):($2) axes x1y1 with lines linewidth 2 lt 2 lc "black" title "VM frozen", \
+     ""		index 11 using ($1 * 1000 - 11000):($2) axes x1y1 with lines linewidth 2 lt 3 lc "black" title "VM resume & server rebind", \
+     ""         index 10 using ($1 * 1000 - 11000):($2) axes x1y1 with lines linewidth 2 lt 1 lc "black" title "Client rebind"
+
+#     		index 1 using ($1 * 1000 - 11000):($2 + 3000 + 10000) axes x1y2 with points ls 7 ps 0.3 lt 7 title "Client Ping CSeq", \
+#     ""         index 2 using ($1 * 1000 - 11000):($2 + 3000 + 10000) axes x1y2 with points ls 7 ps 0.3 lt 7 notitle, \
+#     ""         index 5 using ($1 * 1000 - 11000):($2 + 2000 + 10000) axes x1y2 with points ls 7 ps 0.3 lt 5 title "Server Ping CSeq", \
+#     ""         index 6 using ($1 * 1000 - 11000):($2 + 2000 + 10000) axes x1y2 with points ls 7 ps 0.3 lt 5 notitle, \
--- migration/plot.py
+++ migration/plot.py
@@ -3,6 +3,8 @@
 import re
 import os
 
+pat_time = re.compile(r'^(\d+): .*$')
+
 pat_client_ping = re.compile(r'^(\d+): PING sent; client seq = (\d+)$')
 pat_client_pong = re.compile(r'^(\d+): PONG received; client seq = (\d+), server seq = (\d+)$')
 pat_client_update = re.compile(r'^(\d+): updating XIAPingSource with new address .*$')
@@ -11,10 +13,12 @@ pat_server_ping = re.compile(r'^(\d+): PING received; client seq = (\d+)$')
 pat_server_pong = re.compile(r'^(\d+): PONG sent; client seq = (\d+), server seq = (\d+)$')
 pat_server_update = re.compile(r'^(\d+): updating XIAPingResponder with new address .*$')
 
-# use 250 ms average
-bucket_size = 0.250
-# use 1 out of 10 points
-sampling_rate = 10
+#bucket_size = 0.050 # in seconds
+averaging_window = 0.250
+averaging_window_step = 0.001
+sampling_rate = 1
+
+rtt = 0.200 / 1000      # known rtt between client & server
 
 def process(f_client, f_server):
     current_ping_dst = 0
@@ -25,11 +29,15 @@ def process(f_client, f_server):
     server_first_t = None
     server_first_seq = None
     last = 0
+    last_reception = 0
+    freeze_t = None
 
     client_ping = [[], []]
     client_pong = [[], []]
     server_ping = [[], []]
     server_pong = [[], []]
+    client_update = []
+    server_update = []
 
     output_client = list(f_client.readlines())
     output_server = list(f_server.readlines())
@@ -50,19 +58,45 @@ def process(f_client, f_server):
 
             client_seq = int(mat.group(2))
             if client_first_seq == client_seq:
-                client_first_t = int(mat.group(1)) / 1000000.
+                client_first_t = int(mat.group(1)) / 1000000. - rtt / 2.
                 break
         break
 
-    for line in output_client + output_server:
-        line = line.strip()
+    while True:
+        if output_client and output_server:
+            mat_client = pat_time.match(output_client[0].strip())
+            if mat_client is None:
+                del output_client[0]
+                continue
+
+            mat_server = pat_time.match(output_server[0].strip())
+            if mat_server is None:
+                del output_server[0]
+                continue
+
+            client_t = int(mat_client.group(1)) / 1000000. - client_first_t
+            server_t = int(mat_server.group(1)) / 1000000. - server_first_t
+
+            sel_client = client_t < server_t
+        elif output_client:
+            sel_client = True
+        elif output_server:
+            sel_client = False
+        else:
+            break
+
+        if sel_client:
+            line = output_client[0].strip()
+            del output_client[0]
+        else:
+            line = output_server[0].strip()
+            del output_server[0]
 
         mat = pat_client_ping.match(line)
         if mat is not None:
             t = int(mat.group(1)) / 1000000. - client_first_t
             seq = int(mat.group(2)) - client_first_seq
             if t >= 0: client_ping[current_ping_dst].append((t, seq))
-            if last < t: last = t
 
         mat = pat_client_pong.match(line)
         if mat is not None:
@@ -70,6 +104,9 @@ def process(f_client, f_server):
             seq = int(mat.group(2)) - client_first_seq
             seq2 = int(mat.group(3)) - server_first_seq
             if t >= 0: client_pong[current_pong_src].append((t, seq, seq2))
+            if last_reception > 0 and t - last_reception > 0.100 and freeze_t is None:
+                freeze_t = last_reception
+            else: last_reception = t
             if last < t: last = t
 
         mat = pat_server_ping.match(line)
@@ -89,47 +126,90 @@ def process(f_client, f_server):
 
         mat = pat_client_update.match(line)
         if mat is not None:
+            t = int(mat.group(1)) / 1000000. - client_first_t
+            if t >= 0: client_update.append(t)
             current_ping_dst = 1 - current_ping_dst
 
         mat = pat_server_update.match(line)
         if mat is not None:
+            t = int(mat.group(1)) / 1000000. - server_first_t
+            if t >= 0: server_update.append(t)
             current_pong_src = 1 - current_pong_src
 
     # calculate avg rate
-    buckets = []
-    for v in client_pong[0] + client_pong[1]:
-        bucket_idx = int(v[0] / bucket_size)
-        if len(buckets) <= bucket_idx:
-            buckets += [0.] * (bucket_idx - len(buckets) + 1)
-        buckets[bucket_idx] += 1. / bucket_size
+    #buckets = []
+    #for v in client_pong[0] + client_pong[1]:
+    #    bucket_idx = int(v[0] / bucket_size)
+    #    if len(buckets) <= bucket_idx:
+    #        buckets += [0.] * (bucket_idx - len(buckets) + 1)
+    #    buckets[bucket_idx] += 1. / bucket_size
+    rates = []
+    window = []
+    t = 0
+    rate_points = list(sorted(client_pong[0] + client_pong[1]))
+    while rate_points:
+        while window and t - window[0][0] > averaging_window:
+            del window[0]
+        while rate_points:
+            if rate_points[0][0] <= t:
+                window.append(rate_points[0])
+                del rate_points[0]
+            else:
+                break
+        if window:
+            current_rate = len(window) / averaging_window
+            rates.append((t, current_rate))
+        t += averaging_window_step
 
     # write avg rate & points
     f = open('plot.dat', 'w')
 
-    for bucket_idx, rate in enumerate(buckets):
-        f.write('%f %f\n' % (bucket_idx * bucket_size, rate))
+    #for bucket_idx, rate in enumerate(buckets):
+    #    f.write('%f %f\n' % ((bucket_idx + 0.5) * bucket_size, rate))
+    #f.write('\n\n')
+    for t, rate in rates:
+        f.write('%f %f\n' % (t, rate))
     f.write('\n\n')
 
-    for group in client_ping:
+    for i, group in enumerate(client_ping):
+        f.write('# client_ping %d\n' % i)
         for t, seq in group:
             if seq % sampling_rate == 0:
                 f.write('%f %d\n' % (t, seq))
         f.write('\n\n')
-    for group in client_pong:
+    for i, group in enumerate(client_pong):
+        f.write('# client_pong %d\n' % i)
         for t, seq, seq2 in group:
             if seq % sampling_rate == 0:
                 f.write('%f %d %d\n' % (t, seq, seq2))
         f.write('\n\n')
-    for group in server_ping:
+    for i, group in enumerate(server_ping):
+        f.write('# server_ping %d\n' % i)
         for t, seq in group:
             if seq % sampling_rate == 0:
                 f.write('%f %d\n' % (t, seq))
         f.write('\n\n')
-    for group in server_pong:
+    for i, group in enumerate(server_pong):
+        f.write('# server_pong %d\n' % i)
         for t, seq, seq2 in group:
             if seq % sampling_rate == 0:
                 f.write('%f %d %d\n' % (t, seq, seq2))
         f.write('\n\n')
+
+    for t in [freeze_t]:
+        f.write('%f 0\n' % t)
+        f.write('%f 100000\n' % t)
+    f.write('\n\n')
+
+    for t in client_update:
+        f.write('%f 0\n' % t)
+        f.write('%f 100000\n' % t)
+    f.write('\n\n')
+
+    for t in server_update:
+        f.write('%f 0\n' % t)
+        f.write('%f 100000\n' % t)
+    f.write('\n\n')
 
 
 if __name__ == '__main__':
