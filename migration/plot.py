d1821001e8da63ab8208cef0c580592401843260
Hyeontaek Lim <hl@cs.cmu.edu>
2011-09-23 16:09:23 -0400

    updated timeline plotting script

--- migration/plot.py
+++ migration/plot.py
@@ -3,77 +3,136 @@
 import re
 import os
 
-pat_ping = re.compile(r'^(\d+): PING sent; client seq = (\d+)$')
-pat_pong = re.compile(r'^(\d+): PONG received; client seq = (\d+), server seq = (\d+)$')
-pat_update = re.compile(r'^updating XIAPingSource with new address .*$')
+pat_client_ping = re.compile(r'^(\d+): PING sent; client seq = (\d+)$')
+pat_client_pong = re.compile(r'^(\d+): PONG received; client seq = (\d+), server seq = (\d+)$')
+pat_client_update = re.compile(r'^(\d+): updating XIAPingSource with new address .*$')
 
-# ignore first & last parts
-trim_left = 1.0
-trim_right = 1.0
-# use 100 ms average
-bucket_size = 0.100
+pat_server_ping = re.compile(r'^(\d+): PING received; client seq = (\d+)$')
+pat_server_pong = re.compile(r'^(\d+): PONG sent; client seq = (\d+), server seq = (\d+)$')
+pat_server_update = re.compile(r'^(\d+): updating XIAPingResponder with new address .*$')
 
-def process(f):
+# use 250 ms average
+bucket_size = 0.250
+# use 1 out of 10 points
+sampling_rate = 10
+
+def process(f_client, f_server):
     current_ping_dst = 0
     current_pong_src = 0
 
-    first = None
+    client_first_t = None
+    client_first_seq = None
+    server_first_t = None
+    server_first_seq = None
+    last = 0
 
-    ping_to = [[], []]
-    pong_from = [[], []]
+    client_ping = [[], []]
+    client_pong = [[], []]
+    server_ping = [[], []]
+    server_pong = [[], []]
 
-    for line in f:
+    output_client = list(f_client.readlines())
+    output_server = list(f_server.readlines())
+
+    for line in output_server:
+        mat = pat_server_pong.match(line.strip())
+        if mat is None:
+            continue
+
+        server_first_t = int(mat.group(1)) / 1000000.
+        client_first_seq = int(mat.group(2))
+        server_first_seq = int(mat.group(3))
+
+        for line in output_client:
+            mat = pat_client_ping.match(line.strip())
+            if mat is None:
+                continue
+
+            client_seq = int(mat.group(2))
+            if client_first_seq == client_seq:
+                client_first_t = int(mat.group(1)) / 1000000.
+                break
+        break
+
+    for line in output_client + output_server:
         line = line.strip()
 
-        mat = pat_ping.match(line)
+        mat = pat_client_ping.match(line)
         if mat is not None:
-            t = int(mat.group(1))
-            if first is None:
-                first = t + trim_left
-            t = (t - first) / 1000000.
-            if t >= 0:
-                ping_to[current_ping_dst].append(t)
+            t = int(mat.group(1)) / 1000000. - client_first_t
+            seq = int(mat.group(2)) - client_first_seq
+            if t >= 0: client_ping[current_ping_dst].append((t, seq))
+            if last < t: last = t
 
-        mat = pat_pong.match(line)
+        mat = pat_client_pong.match(line)
         if mat is not None:
-            t = int(mat.group(1))
-            t = (t - first) / 1000000.
-            if t >= 0:
-                pong_from[current_pong_src].append(t)
+            t = int(mat.group(1)) / 1000000. - client_first_t
+            seq = int(mat.group(2)) - client_first_seq
+            seq2 = int(mat.group(3)) - server_first_seq
+            if t >= 0: client_pong[current_pong_src].append((t, seq, seq2))
+            if last < t: last = t
 
-        mat = pat_update.match(line)
+        mat = pat_server_ping.match(line)
+        if mat is not None:
+            t = int(mat.group(1)) / 1000000. - server_first_t
+            seq = int(mat.group(2)) - client_first_seq
+            if t >= 0: server_ping[current_ping_dst].append((t, seq))
+            if last < t: last = t
+
+        mat = pat_server_pong.match(line)
+        if mat is not None:
+            t = int(mat.group(1)) / 1000000. - server_first_t
+            seq = int(mat.group(2)) - client_first_seq
+            seq2 = int(mat.group(3)) - server_first_seq
+            if t >= 0: server_pong[current_pong_src].append((t, seq, seq2))
+            if last < t: last = t
+
+        mat = pat_client_update.match(line)
         if mat is not None:
             current_ping_dst = 1 - current_ping_dst
+
+        mat = pat_server_update.match(line)
+        if mat is not None:
             current_pong_src = 1 - current_pong_src
 
     # calculate avg rate
     buckets = []
-    for v in pong_from[0] + pong_from[1]:
-        bucket_idx = int(v / bucket_size)
+    for v in client_pong[0] + client_pong[1]:
+        bucket_idx = int(v[0] / bucket_size)
         if len(buckets) <= bucket_idx:
             buckets += [0.] * (bucket_idx - len(buckets) + 1)
         buckets[bucket_idx] += 1. / bucket_size
 
-    # drop last part of data
-    last = max(ping_to[0] + ping_to[1])
-    ping_to[0] = filter(lambda x: last - x > trim_right, ping_to[0])
-    ping_to[1] = filter(lambda x: last - x > trim_right, ping_to[1])
-    pong_from[0] = filter(lambda x: last - x > trim_right, pong_from[0])
-    pong_from[1] = filter(lambda x: last - x > trim_right, pong_from[1])
-    buckets = buckets[:-int(trim_right / bucket_size)]
-
     # write avg rate & points
     f = open('plot.dat', 'w')
+
     for bucket_idx, rate in enumerate(buckets):
         f.write('%f %f\n' % (bucket_idx * bucket_size, rate))
     f.write('\n\n')
-    f.write('\n'.join([str(x) for x in ping_to[0]]) + '\n\n\n')
-    f.write('\n'.join([str(x) for x in ping_to[1]]) + '\n\n\n')
-    f.write('\n'.join([str(x) for x in pong_from[0]]) + '\n\n\n')
-    f.write('\n'.join([str(x) for x in pong_from[1]]) + '\n\n\n')
+
+    for group in client_ping:
+        for t, seq in group:
+            if seq % sampling_rate == 0:
+                f.write('%f %d\n' % (t, seq))
+        f.write('\n\n')
+    for group in client_pong:
+        for t, seq, seq2 in group:
+            if seq % sampling_rate == 0:
+                f.write('%f %d %d\n' % (t, seq, seq2))
+        f.write('\n\n')
+    for group in server_ping:
+        for t, seq in group:
+            if seq % sampling_rate == 0:
+                f.write('%f %d\n' % (t, seq))
+        f.write('\n\n')
+    for group in server_pong:
+        for t, seq, seq2 in group:
+            if seq % sampling_rate == 0:
+                f.write('%f %d %d\n' % (t, seq, seq2))
+        f.write('\n\n')
 
 
 if __name__ == '__main__':
-    process(open('output'))
+    process(open('output_client'), open('output_server'))
     os.system('./plot.gnuplot')
 
--- migration/run_ping_client.sh
+++ migration/run_ping_client.sh
@@ -7,7 +7,7 @@ source $PREFIX/common.sh
 sudo killall click
 rm -f output
 
-tail -F output | grep "client seq = .*000," &
-sudo $CLICK_PATH/userlevel/click $CLICK_PATH/conf/xia/xia_vm_ping_client.click > output 2>&1
+tail -F output_client | grep "client seq = .*000," &
+sudo $CLICK_PATH/userlevel/click $CLICK_PATH/conf/xia/xia_vm_ping_client.click > output_client 2>&1
 kill %1
 
