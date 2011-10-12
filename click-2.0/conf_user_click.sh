17b5c8e4f109477199cb8f01124e305411120885
Hyeontaek Lim <hl@cs.cmu.edu>
2011-10-12 15:55:12 -0400

    removed tcmalloc dependancy; moved ioengine compilation from load_user_click.sh to conf_user_click.sh

--- click-2.0/conf_user_click.sh
+++ click-2.0/conf_user_click.sh
@@ -4,5 +4,7 @@ IO_ENGINE=`dirname $0`/../io_engine
 IO_ENGINE=`readlink -f $IO_ENGINE`
 
 cd `dirname $0`/
-CPPFLAGS="$CPPFLAGS -I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib -ltcmalloc_minimal" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel --enable-multithread=24  --enable-ip6 --enable-ipsec "$*"
+CPPFLAGS="$CPPFLAGS -I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel --enable-multithread=24  --enable-ip6 --enable-ipsec "$*"
 
+make -j24 -s -C $IO_ENGINE/driver || exit 1
+make -j24 -s -C $IO_ENGINE/lib || exit 1
