b4fc45749d5bede92a33f53492a8e3ada1bd811c
Hyeontaek Lim <hl@cs.cmu.edu>
2011-10-17 14:53:35 -0400

    continue compilation even if the driver fails to compile

--- click-2.0/conf_user_click.sh
+++ click-2.0/conf_user_click.sh
@@ -6,5 +6,5 @@ IO_ENGINE=`readlink -f $IO_ENGINE`
 cd `dirname $0`/
 CPPFLAGS="$CPPFLAGS -I$IO_ENGINE/include" LDFLAGS="-L$IO_ENGINE/lib" ./configure --disable-linuxmodule --enable-warp9 --enable-user-multithread --enable-userlevel --enable-multithread=24  --enable-ip6 --enable-ipsec "$*"
 
-make -j24 -s -C $IO_ENGINE/driver || exit 1
+make -j24 -s -C $IO_ENGINE/driver
 make -j24 -s -C $IO_ENGINE/lib || exit 1
