2459338c769ca31fbb45401747d46cb840e7b391
Suk-Bok Lee <sbl@cs.cmu.edu>
2011-10-27 01:54:04 -0400

    demo automated scripts

---
+++ click-2.0/conf/geni/xia-demo-scripts/network_evolvability.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./sub_scripts/setup_click_routers__network_evolavability.sh
+./sub_scripts/setup_service__network_evolavability.sh
+./sub_scripts/setup_proxy__network_evolavability.sh
+./sub_scripts/setup_visualizer__network_evolavability.sh
---
+++ click-2.0/conf/geni/xia-demo-scripts/sub_scripts/setup_service__end_host_evolavability.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ssh -A mberman@pg40.emulab.net -f  "cd xia-core/proxies; sleep 5; sudo killall -9 webserver.py; ./webserver.py; exit "
+
+ssh -A mberman@pg40.emulab.net -f  "cd xia-core/web_demo; sleep 5; sudo killall -9 stock_service.py; ./stock_service.py; exit "
+
+ssh -A mberman@pg40.emulab.net -f  "cd xia-core/XIASocket/sample; sleep 5; sudo killall -9 vs; ./vs video.ogv; exit "
+
