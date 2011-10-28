0443fd53e26c8a19318d23a3387628ff594877b4
dongsuh <dongsuh@dongsuh-XPS-M1330.(none)>
2011-10-28 01:59:35 -0400

    some weird interaction spotted (BUG report?)

--- web_demo/start_demo_service.sh
+++ web_demo/start_demo_service.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
+# local script 
+
 killall -9 stock_service.py
 killall -9 vs
 killall -9 proxy.py 
+killall -9 webserver.py
+
 ./stock_service.py &
 cd ../XIASocket/sample/
 ./vs video.ogv &
 
+sleep 10  # need this otherwise it doesn't work
+
 cd ../../proxies
+./webserver.py &
+sleep 1
 ./proxy.py &
