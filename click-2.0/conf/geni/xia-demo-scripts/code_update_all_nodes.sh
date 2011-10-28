c8f684789d8b0a562daab8f24c94d39b042a6843
Suk-Bok Lee <sbl@cs.cmu.edu>
2011-10-27 22:09:02 -0400

    fixed permission error

--- click-2.0/conf/geni/xia-demo-scripts/code_update_all_nodes.sh
+++ click-2.0/conf/geni/xia-demo-scripts/code_update_all_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for host in [ "pg40", "pg56", "pg42", "pg55"]
+for host in pg40 pg56 pg42 pg55
 do
 ssh -A $host "~/xia-core/click-2.0/conf/geni/xia-demo-scripts/sub_scripts/code_update.sh"
 done
