db52fd00bfdf9fdb17268a0c049ae4efd9df6205
David Naylor <dnaylor@cs.cmu.edu>
2011-10-30 12:24:30 -0400

    Can call start_demo_service.sh with -r (restart) flag to tell webserver to not republish content

--- web_demo/start_demo_service.sh
+++ web_demo/start_demo_service.sh
@@ -7,12 +7,12 @@ killall -9 vs
 killall -9 proxy.py 
 killall -9 webserver.py
 
-./stock_service.py &
+./stock_service.py  > /dev/null &
 cd ../XIASocket/sample/
-./vs video.ogv &
+./vs video.ogv > /dev/null &
 sleep 1  # need this otherwise it doesn't work
 
 cd ../../proxies
-./webserver.py &
+./webserver.py $@ &
 sleep 1
-./proxy.py &
+./proxy.py > /dev/null &
