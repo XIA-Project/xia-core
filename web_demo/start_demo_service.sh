7b18614d73cef26ce716943af206ae1a90cabfca
Dongsu Han <dongsuh@cs.cmu.edu>
2011-11-04 14:48:02 -0400

    Instructions for running the demo locally

--- web_demo/start_demo_service.sh
+++ web_demo/start_demo_service.sh
@@ -6,7 +6,14 @@ killall -9 stock_service.py
 killall -9 vs
 killall -9 proxy.py 
 killall -9 webserver.py
+sleep 1
 
+killall -9 stock_service.py
+killall -9 vs
+killall -9 proxy.py 
+killall -9 webserver.py
+
+sleep 1
 ./stock_service.py &
 cd ../XIASocket/sample/
 ./vs video.ogv &
@@ -15,4 +22,4 @@ sleep 1  # need this otherwise it doesn't work
 cd ../../proxies
 ./webserver.py $@ &
 sleep 1
-./proxy.py &
+./proxy.py 8080 127.0.0.1 &
