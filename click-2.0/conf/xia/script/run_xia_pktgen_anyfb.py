b4f46220d46e988d8a1ab6fb494b9ed8567131bd
Hyeontaek Lim <hl@cs.cmu.edu>
2011-11-04 20:17:14 -0400

    fixed to simply use payload size

--- click-2.0/conf/xia/script/run_xia_pktgen_anyfb.py
+++ click-2.0/conf/xia/script/run_xia_pktgen_anyfb.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 if len(sys.argv) < 3:
-    print 'usage: %s NUM-FALLBACKS PACKET-SIZE [COUNT]' % sys.argv[0]
+    print 'usage: %s NUM-FALLBACKS PAYLOAD-SIZE [COUNT]' % sys.argv[0]
     sys.exit(1)
 
 nfallbacks = int(sys.argv[1])
-packet_size = int(sys.argv[2])
+payload_size = int(sys.argv[2])
 
 script_names = {
     0: 'xia_mq_24_rand.click',
@@ -18,11 +18,6 @@ script_names = {
 }
 
 dir_ = os.path.dirname(os.path.abspath(sys.argv[0]))
-
-assert packet_size % 64 == 0, 'packet size must be a multiple of 64 for high performance'
-
-payload_size = packet_size - (8 + 28 * nfallbacks + 28)
-assert payload_size >= 0, 'too small payload for the given fallbacks'
 
 if len(sys.argv) >= 4:
     count = 'COUNT=%d' % int(sys.argv[3])
