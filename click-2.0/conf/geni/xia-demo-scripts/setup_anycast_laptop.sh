ec90fec89bffdb7ef7f99a071904436a282af416
Suk-Bok Lee <sbl@cs.cmu.edu>
2011-11-08 01:07:47 -0500

    changed permission

---
+++ click-2.0/conf/geni/xia-demo-scripts/setup_anycast_laptop.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+sudo killall -9 click
+
+killall -9 webserver_replicate_cmu.py
+killall -9 stock_service_replicate_cmu.py
+
+killall -9 pc_beacon
+
+sudo ../../../userlevel/click ../cmu-server.click &
+
+sleep 1
+cd ../../../../web_demo/
+
+./stock_service_replicate_cmu.py &
+
+cd ../proxies
+./webserver_replicate_cmu.py $@  &
+
+
+
+#run ps_beacon at pg56
+ssh -A mberman@pg56.emulab.net -f "~/xia-core/click-2.0/conf/geni/xia-demo-scripts/sub_scripts/setup_beacon_pg56.sh"
+
+
+sleep 2
+
+cd ../XIASocket/sample/
+./pc_beacon &
+
+
+#run check_beacon at pg42
+ssh -A mberman@pg42.emulab.net -f "~/xia-core/click-2.0/conf/geni/xia-demo-scripts/sub_scripts/check_beacon_pg42.sh"
+
+
+
+echo "Ready!"
+
+
