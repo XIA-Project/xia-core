94c1287a7d0f340cf370eefcd2359869cc7e32c2
Dan Barrett <barrettd@cs.cmu.edu>
2012-01-23 14:29:44 -0500

    Close sockets when period of inactivity happens. Also fixed issue with file descriptor exhaustion in the main process.

--- XIASocket/sample/echoserver.c
+++ XIASocket/sample/echoserver.c
@@ -28,6 +28,9 @@
 #define AD0   "AD:1000000000000000000000000000000000000000"
 #define SID0 "SID:0f00000000000000000000000000000000000777"
 
+// if no data is received from the client for this number of seconds, close the socket
+#define WAIT_FOR_DATA	20
+
 // global configuration options
 int verbose = 1;
 
@@ -39,8 +42,8 @@ int verbose = 1;
 char *createDAG(const char *ad, const char *host, const char *service)
 {
 	int len = snprintf(NULL, 0, DAG, ad, host, service) + 1;
-    char * dag = (char*)malloc(len);
-    sprintf(dag, DAG, ad, host, service);
+	char * dag = (char*)malloc(len);
+	sprintf(dag, DAG, ad, host, service);
 	return dag;
 }
 
@@ -122,13 +125,63 @@ void die(int ecode, const char *fmt, ...)
 	exit(ecode);
 }
 
+/*
+** get data from the client and return it
+** run in a child process that was forked off from the main process.
+*/
+void process(int sock)
+{
+	char buf[2048];
+	int n;
+	pid_t pid = getpid();
+
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(sock, &fds);
+
+	struct timeval tv;
+	tv.tv_sec = WAIT_FOR_DATA;
+	tv.tv_usec = 0;
+
+   	while (1) {
+		memset(buf, 0, sizeof(buf));
+
+		 if ((n = select(sock + 1, &fds, NULL, NULL, &tv)) < 0) {
+			 warn("%5d Select failed, closing...\n", pid);
+			 break;
+		 
+		 } else if (n == 0) {
+			 // we timed out, close the socket
+			 say("%5d timed out on recv\n", pid);
+			 break;
+		 } else if (!FD_ISSET(sock, &fds)) {
+			 // this shouldn't happen!
+			 die(-4, "something is really wrong, exiting\n");
+		 }
+	
+		if ((n = Xrecv(sock, buf, sizeof(buf), 0)) < 0) {
+			warn("Recv error on socket %d, closing connection\n", pid);
+			break;
+		}
+			
+		say("%5d received %d bytes\n", pid, n);
+
+		if ((n = Xsend(sock, buf, n, 0)) < 0) {
+			warn("%5d send error\n", pid);
+			break;
+		}
+
+		say("%5d sent %d bytes\n", pid, n);
+   	}
+	say("%5d closing\n", pid);
+	Xclose(sock);
+}
+
+
 int main(int argc, char *argv[])
 {
-    int acceptor, sock;
-    char buf[2048];
+	int acceptor, sock;
 	char *dag;
-	int n;
-    pid_t pid;
 
 	getConfig(argc, argv);
 
@@ -139,53 +192,41 @@ int main(int argc, char *argv[])
 	}
 	say("Created DAG: \n%s\n", dag);
 
-    if ((acceptor = Xsocket(XSOCK_STREAM)) < 0) {
+	if ((acceptor = Xsocket(XSOCK_STREAM)) < 0) {
 		die(-2, "unable to create the server socket\n");
 	}
 
-    if (Xbind(acceptor, dag) < 0) {
+	if (Xbind(acceptor, dag) < 0) {
 		die(-3, "unable to bind to the dag\n");
 	}
 
-    while (1) {
-    
+	while (1) {
+
 		say("Xsock %4d waiting for a new connection.\n", acceptor);
-    	if ((sock = Xaccept(acceptor)) < 0) {
+		if ((sock = Xaccept(acceptor)) < 0) {
 			warn("Xsock %d accept failure! error = %d\n", acceptor, errno);
+			// FIXME: should we die here instead or try and recover?
 			continue;
 		}
 
 		say ("Xsock %4d new session\n", sock);
-    	
-    	pid = fork();
-    
-    	if (pid == 0) {  
-    		
-    		while (1) {
-				memset(buf, 0, sizeof(buf));
-			
-				if ((n = Xrecv(sock, buf, sizeof(buf), 0)) < 0) {
-					warn("Recv error on socket %d, closing connection\n", sock);
-					break;
-				}
-			
-				say("Xsock %4d received %d bytes\n", sock, n);
 
-				if ((n = Xsend(sock, buf, n, 0)) < 0) {
-					warn("send error on socket %d\n", sock);
-					break;
-				}
+		pid_t pid = fork();
 
-				say("Xsock %4d sent %d bytes\n", sock, n);
-    		}
-			printf("Xsock %4d closing\n", sock);
-			Xclose(sock);
+		if (pid == 0) {  
+			process(sock);
 			return 0;
-    	}
-    }
+
+		} else {
+			// FIXME: we need to close the socket in the main process or the file
+			// descriptor limit will be hit. But if Xclose is called, the connection
+			// is torn down in click as well keeping the child process from using it.
+			// for now use a regular close to shut it here without affecting the child.
+			close(sock);
+		}
+	}
 
 	free(dag);
 	Xclose(acceptor);
-    return 0;
+	return 0;
 }
-
