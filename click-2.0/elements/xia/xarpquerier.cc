74ae561315d41c71f0d9e3825d03739981a6e860
Suk-Bok Lee <sbl@cs.cmu.edu>
2012-03-16 10:38:57 -0400

    copyright restored

--- click-2.0/elements/xia/xarpquerier.cc
+++ click-2.0/elements/xia/xarpquerier.cc
@@ -1,5 +1,20 @@
 /*
  * xarpquerier.{cc,hh} -- XARP resolver element
+ * Robert Morris, Eddie Kohler
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ * Copyright (c) 2005 Regents of the University of California
+ * Copyright (c) 2008-2009 Meraki, Inc. 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
  */
  
 // Modified from original ARP code 
