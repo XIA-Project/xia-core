09773f0c7d8ebc57cc5c4a61d0080e301857b534
Suk-Bok Lee <sbl@cs.cmu.edu>
2012-04-16 18:12:23 -0400

    minor fix for geni init scripts

--- click-2.0/conf/geni/init-scripts/sub/click_host_start.sh
+++ click-2.0/conf/geni/init-scripts/sub/click_host_start.sh
@@ -2,5 +2,5 @@
 
 cd ~/xia-core/click-2.0 
 sudo killall -9 click 
-sudo userlevel/click xia-core/click-2.0/conf/geni/init-scripts/template/host.click >& /dev/null &
+sudo userlevel/click conf/geni/init-scripts/template/host.click >& /dev/null &
 
