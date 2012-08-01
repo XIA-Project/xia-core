9a6ecf6cd623028b1d6671b3b9e22a7cd01316c7
David Naylor <dnaylor@cs.cmu.edu>
2012-08-01 17:06:19 -0400

    Updated dual stack host click config generation script

--- tools/click_templates/xconfig.py
+++ tools/click_templates/xconfig.py
@@ -93,6 +93,8 @@ def getInterfaces(ignore_interfaces, specific_interface):
 	temp_array = result.split("\n")
 	iface, mac, ip = (None, None, None)
 	for i in range(len(temp_array)):
+		if temp_array[i].strip() == '':
+			continue
 		if i % 3 == 0:
 			iface = temp_array[i].split(' ')[0]
 			mac = temp_array[i].split(' ')[4]
@@ -315,21 +317,130 @@ def makeRouterConfig(ad, hid):
 	f.write(newtext)
 	f.close()
 
+##
+## fill in the dual-stack host config
+##
+## the router config file is broken into 4 sections
+## header - router definition and include lines
+## used ports - connects used ports to interfaces (currently we just use port 0)
+## extra section - discard mappping for unused ports on the 
+##  router (currently ports 1-3)
+## footer - boilerplate
+#
+#def makeDualHostConfig(ad, hid, rhid):
+#	global interface_filter, interface
+#
+#	interfaces = getInterfaces(interface_filter, interface) 
+#	print 'Making dual stack host config'
+#
+#	try:
+#		f = open(dualhostconfig + "." + ext, "r")
+#		text = f.read()
+#		f.close()
+#
+#		f = open(dualhostconfig, "w")
+#
+#	except:
+#		print "error opening file for reading and/or writing"
+#		sys.exit(-1)
+#
+#	(header, body, extra, footer) = text.split("######")
+#
+#	tpl = Template(header)
+#
+#	xchg = {}
+#	if (nameserver == "no"):
+#		xchg['ADNAME'] = ad
+#		xchg['HID'] = hid
+#	else:
+#		xchg['ADNAME'] = nameserver_ad		
+#		xchg['HID'] = nameserver_hid	
+#	xchg['RHID'] = rhid
+#	xchg['HNAME'] = getHostname()
+#	
+#	if ip_override_addr == None:
+#		xchg['EXTERNAL_IP'] = '0.0.0.0'
+#	else:
+#		xchg['EXTERNAL_IP'] = ip_override_addr
+#
+#	# create $MAC0 thru $MAC3 replacements
+#	i = 0  
+#	while i < 3 and i < len(interfaces):  # Only go to 2 because the host is connected to router port 3
+#		repl = 'MAC' + str(i)
+#		xchg[repl] = interfaces[i][1]
+#		repl = 'IPADDR' + str(i)
+#		xchg[repl] = interfaces[i][2]
+#		repl = 'GWADDR' + str(i)
+#		xchg[repl] = interfaces[i][3]
+#		repl = 'EXT_IPADDR' + str(i)
+#		xchg[repl] = interfaces[i][4]
+#		i += 1
+#	while i < 3:
+#		repl = 'MAC' + str(i)
+#		xchg[repl] = "00:00:00:00:00:00"
+#		repl = 'IPADDR' + str(i)
+#		xchg[repl] = "1.1.1.1"
+#		repl = 'GWADDR' + str(i)
+#		xchg[repl] = "1.1.1.1"
+#		repl = 'EXT_IPADDR' + str(i)
+#		xchg[repl] = "1.1.1.1"
+#		i += 1
+#	 	
+#	newtext = tpl.substitute(xchg)
+#
+#	i = 0
+#	tpl = Template(body)
+#	for (interface, mac, ip, gw, ext_ip) in interfaces:
+#		xchg['IFACE'] = interfaces[i][0]
+#		xchg['MAC'] = interfaces[i][1]
+#		xchg['NUM'] = i
+##		xchg['NUMa'] = str(i) + "a"
+##		xchg['NUMb'] = str(i) + "b"
+#		i += 1
+#
+#		newtext += tpl.substitute(xchg)
+#
+#		if i >= 3:
+#			break
+#
+#	tpl = Template(extra)
+#	while i < 3:
+#		xchg['NUM'] = i
+#		newtext += tpl.substitute(xchg)
+#		i += 1
+#
+#	tpl = Template(footer)
+#	newtext += tpl.substitute(xchg)
+#
+#	f.write(newtext)
+#	f.close()
+
+
 #
 # fill in the dual-stack host config
 #
+# The dual-stack hosts consists of a host element
+# connected to a dual-stack router. On the router,
+# for now we assume that port 3 is connected to IP,
+# port 2 is connected to the host, and the
+# remaining ports are idle or connected to XIA networks
+#
 # the router config file is broken into 4 sections
 # header - router definition and include lines
-# used ports - connects used ports to interfaces (currently we just use port 0)
-# extra section - discard mappping for unused ports on the 
-#  router (currently ports 1-3)
+# used ports - connects used ports to interfaces 
+# extra section - discard mappping for unused ports on the router 
 # footer - boilerplate
 
 def makeDualHostConfig(ad, hid, rhid):
 	global interface_filter, interface
 
-	interfaces = getInterfaces(interface_filter, interface) 
-	print 'Making dual stack host config'
+	if interface_filter == None:
+		filter = interface
+	else:
+		filter = '%s,%s' % (interface, interface_filter)
+
+	ip_interface = getInterfaces(None, interface)
+	xia_interfaces = getInterfaces(filter, None) 
 
 	try:
 		f = open(dualhostconfig + "." + ext, "r")
@@ -352,28 +463,29 @@ def makeDualHostConfig(ad, hid, rhid):
 		xchg['HID'] = hid
 	else:
 		xchg['ADNAME'] = nameserver_ad		
-		xchg['HID'] = nameserver_hid	
+		xchg['HID'] = nameserver_hid
 	xchg['RHID'] = rhid
 	xchg['HNAME'] = getHostname()
 	
-	if ip_override_addr == None:
-		xchg['EXTERNAL_IP'] = '0.0.0.0'
-	else:
-		xchg['EXTERNAL_IP'] = ip_override_addr
+	xchg['EXTERNAL_IP'] = ip_interface[0][4]
 
-	# create $MAC0 thru $MAC3 replacements
+	# Handle the pure XIA ports (0-1)
 	i = 0  
-	while i < 3 and i < len(interfaces):  # Only go to 2 because the host is connected to router port 3
+	while i < 2 and i < len(xia_interfaces):  # Only go to 1 because port 2 is connected to the host and 3 is connected to IP
+		repl = 'IP_ACTIVE' + str(i)
+		xchg[repl] = str(0)
 		repl = 'MAC' + str(i)
-		xchg[repl] = interfaces[i][1]
+		xchg[repl] = xia_interfaces[i][1]
 		repl = 'IPADDR' + str(i)
-		xchg[repl] = interfaces[i][2]
+		xchg[repl] = xia_interfaces[i][2]
 		repl = 'GWADDR' + str(i)
-		xchg[repl] = interfaces[i][3]
+		xchg[repl] = xia_interfaces[i][3]
 		repl = 'EXT_IPADDR' + str(i)
-		xchg[repl] = interfaces[i][4]
+		xchg[repl] = xia_interfaces[i][4]
 		i += 1
-	while i < 3:
+	while i < 2:
+		repl = 'IP_ACTIVE' + str(i)
+		xchg[repl] = str(0)
 		repl = 'MAC' + str(i)
 		xchg[repl] = "00:00:00:00:00:00"
 		repl = 'IPADDR' + str(i)
@@ -383,26 +495,36 @@ def makeDualHostConfig(ad, hid, rhid):
 		repl = 'EXT_IPADDR' + str(i)
 		xchg[repl] = "1.1.1.1"
 		i += 1
+
+	# Handle the IP port (3)
+	xchg['IP_ACTIVE3'] = str(1)
+	xchg['MAC3'] = ip_interface[0][1];
+	xchg['IPADDR3'] = ip_interface[0][2]
+	xchg['GWADDR3'] = ip_interface[0][3]
+	xchg['EXT_IPADDR3'] = ip_interface[0][4]
 	 	
 	newtext = tpl.substitute(xchg)
 
 	i = 0
 	tpl = Template(body)
-	for (interface, mac, ip, gw, ext_ip) in interfaces:
-		xchg['IFACE'] = interfaces[i][0]
-		xchg['MAC'] = interfaces[i][1]
+	for (interface, mac, ip, gw, ext_ip) in xia_interfaces:
+		xchg['IFACE'] = xia_interfaces[i][0]
+		xchg['MAC'] = xia_interfaces[i][1]
 		xchg['NUM'] = i
-#		xchg['NUMa'] = str(i) + "a"
-#		xchg['NUMb'] = str(i) + "b"
 		i += 1
 
 		newtext += tpl.substitute(xchg)
 
-		if i >= 3:
+		if i >= 2:
 			break
 
+	xchg['IFACE'] = ip_interface[0][0]
+	xchg['MAC'] = ip_interface[0][1]
+	xchg['NUM'] = 3
+	newtext += tpl.substitute(xchg)
+
 	tpl = Template(extra)
-	while i < 3:
+	while i < 2:
 		xchg['NUM'] = i
 		newtext += tpl.substitute(xchg)
 		i += 1
@@ -436,7 +558,6 @@ def makeDualRouterConfig(ad, rhid):
 
 	ip_interface = getInterfaces(None, interface)
 	xia_interfaces = getInterfaces(filter, None) 
-	print 'Making dual stack router config'
 
 	try:
 		f = open(dualrouterconfig + "." + ext, "r")
@@ -461,10 +582,7 @@ def makeDualRouterConfig(ad, rhid):
 	xchg['RHID'] = rhid
 	xchg['HNAME'] = getHostname()
 	
-	if ip_override_addr == None:
-		xchg['EXTERNAL_IP'] = '0.0.0.0'
-	else:
-		xchg['EXTERNAL_IP'] = ip_override_addr
+	xchg['EXTERNAL_IP'] = ip_interface[0][4]
 
 	# Handle the pure XIA ports (0-2)
 	i = 0  
