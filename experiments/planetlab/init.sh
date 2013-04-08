b794a1d31da61e968a2e00325a648026d176ac44
Matt Mukerjee <mukerjee@cs.cmu.edu>
2013-04-08 18:18:35 -0400

    Added planetlab scripts

---
+++ experiments/planetlab/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~
+wget http://www.cs.cmu.edu/~mmukerje/planetlab/id_rsa
+chmod 600 id_rsa
+mkdir .ssh
+mv id_rsa .ssh/
+echo "
+cd ~
+mkdir fedora-bin
+cd fedora-bin
+rsync --rsh='ssh -o StrictHostKeyChecking=no -p5556' update@gs11698.sp.cs.cmu.edu:~/fedora/xia-core/experiments/planetlab/bin-files ./
+rsync --rsh='ssh -o StrictHostKeyChecking=no -p5556' -a --files-from=./bin-files update@gs11698.sp.cs.cmu.edu:~/fedora/ ./
+./xia-core/experiments/planetlab/refresh.sh
+" > sync.sh
+chmod 755 sync.sh
+./sync.sh
+#echo '1 * * * * /home/cmu_xia/sync.sh' | crontab -
+
---
+++ experiments/planetlab/refresh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp ./xia-core/experiments/planetlab/libprotobuf.so.8 /usr/local/lib/
+sudo cp ./xia-core/experiments/planetlab/libc.conf /etc/ld.so.conf.d/
+sudo /sbin/ldconfig
