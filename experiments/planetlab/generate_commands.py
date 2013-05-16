4cbd77d96b6543cc495e18f556a037b92e2b48f6
Matt Mukerjee <mukerjee@cs.cmu.edu>
2013-05-16 13:35:09 -0400

    fixed topology generator script

--- experiments/planetlab/generate_commands.py
+++ experiments/planetlab/generate_commands.py
@@ -12,14 +12,21 @@ hostd = dict((x.split('#')[1],(x.split('#')[0],socket.gethostbyname(x.split('#')
 cmd = '[default]\n~/fedora-bin/xia-core/experiments/planetlab/mapper/mapper_client.py red&\n'
 
 machines = open(sys.argv[1],'r').read().split('\n')
+links = []
 for machine in machines:
     name = machine.split(':')[0]
     neighbors = machine.split(':')[1].replace(' ','').split(',')
+    links += [tuple(sorted((name,neighbor))) for neighbor in neighbors]    
+links = list(set(links))
+print links
+
+for machine in machines:
+    name = machine.split(':')[0]
     ip = hostd[name][1]
     host = hostd[name][0]
-    neighbors = [hostd[neighbor][1] for neighbor in neighbors]
     line = '[%s] #%s\nuntil sudo ~/fedora-bin/xia-core/bin/xianet -v -r -P ' % (ip, host)    
-    neighbors = ['%s:400%s' %(neighbor, neighbors.index(neighbor)) for neighbor in neighbors]
+
+    neighbors = ['%s:400%s' % (hostd[''.join([n for n in link if n != name])][1],links.index(link)) for link in links if name in link]    
     line += ','.join(neighbors)
     line += ' -f eth0 start; do echo "restarting click"; done\n'
     cmd += line
