e95565e1ed6697e9f9822efad84723f83b79bfdf
George Nychis <gnychis@gmail.com>
2013-07-23 14:59:03 -0400

    updated preparing script to include check on whether the config file is already patched for Android

--- android-deps/prepare.sh
+++ android-deps/prepare.sh
@@ -4,4 +4,7 @@ if [ ! -d "android-deps/protobuf" ]; then
   svn co -r378 http://protobuf.googlecode.com/svn/trunk/ android-deps/protobuf
 fi
 
-patch -p0 < android-deps/config_patch.diff
+already_patched=$(grep CLICK_ANDROID click/include/click/config.h | wc -l)
+if [ $already_patched -eq 0 ]; then 
+  patch -p0 < android-deps/config_patch.diff
+fi
--- daemons/xhcp/Android.mk
+++ daemons/xhcp/Android.mk
@@ -9,7 +9,7 @@ xhcp_client.cc
 
 LOCAL_CPP_EXTENSION := .c .cc
 
-LCOAL_C_INCLUDES += ../../api/include ../../api/xsocket ../../api/dagaddr ../../click-2.0.1/tools/click-align .
+LCOAL_C_INCLUDES += api/include api/xsocket api/dagaddr click-2.0.1/tools/click-align
 
 LOCAL_SHARED_LIBRARIES := \
 libdagaddr libprotobuf libz libcutils libutils libpthread libclickalign libxiasocket libarmeabi
