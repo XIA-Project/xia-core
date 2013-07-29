c587a6886a318c260c719124b1d62799ddd564c9
Neel Shah <neel@shah7.com>
2013-07-29 12:25:39 -0400

    Adding script to add ifdefs around certain statements for click android

---
+++ android-deps/elements_modifier.py
@@ -0,0 +1,12 @@
+f = open('../click-2.0.1/userlevel/elements.cc', 'r')
+f2= open('../click-2.0.1/userlevel/elementsMod.cc', 'w')
+hotWord = "IPFlowRawSockets"
+hotWord2 = "ipflowrawsockets.hh"
+for line in f:
+	if hotWord in line or hotWord2 in line:
+		f2.write("#ifdef CLICK_ANDROID\n")
+		f2.write("#else\n")
+		f2.write(line)
+		f2.write("#endif\n")
+	else:
+		f2.write(line)
