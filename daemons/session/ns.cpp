6ff6c46ebb06ee8342aaf7449af1a7a14b5fa663
David Naylor <dnaylor@cs.cmu.edu>
2013-08-26 17:00:09 -0400

    Improved logging in IP nameserver

--- daemons/session/ns.cpp
+++ daemons/session/ns.cpp
@@ -9,18 +9,55 @@
 
 using namespace std;
 
-#define DEBUG
+#define PORT 5353
+
+/* LOGGING */
+
+#define V_ERROR 0
+#define V_WARNING 1
+#define V_INFO 2
+#define V_DEBUG 3
 
 #ifdef DEBUG
-#define LOG(s) fprintf(stderr, "%s:%d: INFO  %s\n", __FILE__, __LINE__, s)
-#define LOGF(fmt, ...) fprintf(stderr, "%s:%d: INFO  " fmt"\n", __FILE__, __LINE__, __VA_ARGS__) 
+#define VERBOSITY V_DEBUG
 #else
-#define LOG(s)
-#define LOGF(fmt, ...)
+#define VERBOSITY V_DEBUG
 #endif
-#define ERROR(s) fprintf(stderr, "\033[0;31m%s:%d: ERROR  %s\n\033[0m", __FILE__, __LINE__, s)
-#define ERRORF(fmt, ...) fprintf(stderr, "\033[0;31m%s:%d: ERROR  " fmt"\n\033[0m", __FILE__, __LINE__, __VA_ARGS__) 
 
+#define LOG(levelstr, color, s) fprintf(stderr, "\033[0;3%dm[ %s ]\033[0m\t[%s:%d (thread %p)]\t%s\n", color, levelstr, __FILE__, __LINE__, (void*)pthread_self(), s)
+#define LOGF(levelstr, color, fmt, ...) fprintf(stderr, "\033[0;3%dm[ %s ]\033[0m\t[%s:%d (thread %p)]\t" fmt"\n", color, levelstr, __FILE__, __LINE__, (void*)pthread_self(), __VA_ARGS__) 
+
+#if VERBOSITY >= V_INFO
+#define INFO(s) LOG("INFO", 2, s)
+#define INFOF(fmt, ...) LOGF("INFO", 2, fmt, __VA_ARGS__)
+#else
+#define INFO(s)
+#define INFOF(fmt, ...)
+#endif
+
+#if VERBOSITY >= V_DEBUG
+#define DBG(s) LOG("DEBUG", 4, s)
+#define DBGF(fmt, ...) LOGF("DEBUG", 4, fmt, __VA_ARGS__)
+#else
+#define DBG(s)
+#define DBGF(fmt, ...)
+#endif
+
+#if VERBOSITY >= V_ERROR
+#define ERROR(s) LOG("ERROR", 1, s)
+#define ERRORF(fmt, ...) LOGF("ERROR", 1, fmt, __VA_ARGS__)
+#else
+#define ERROR(s)
+#define ERRORF(fmt, ...)
+#endif
+
+#if VERBOSITY >= V_WARNING
+#define WARN(s) LOG("WARNING", 3, s)
+#define WARNF(fmt, ...) LOGF("WARNING", 3, fmt, __VA_ARGS__)
+#else
+#define WARN(s)
+#define WARNF(fmt, ...)
+#endif
 
 
 map<string, uint32_t> name_to_ip;
@@ -42,13 +79,15 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in addr;
 	addr.sin_family = PF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(5353);
+	addr.sin_port = htons(PORT);
 
 	if (bind(sockfd, (const struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		close(sockfd);
 		ERRORF("bind error: %s", strerror(errno));
 		return -1;
 	}
+
+	INFOF("Listening on port %d", PORT);
 	
 	// listen for REGISTER and LOOKUP messages
 	while (true)
@@ -69,14 +108,14 @@ int main(int argc, char *argv[]) {
 			if (nsm.has_reg()) {  // REGISTER
 
 				session::NSRegister rm = nsm.reg();
-				LOGF("REGISTER: %s", rm.name().c_str());
+				DBGF("REGISTER: %s", rm.name().c_str());
 				name_to_ip[rm.name()] = rm.ip();
 				name_to_port[rm.name()] = rm.port();
 
 			} else if (nsm.has_lookup()) {  // LOOKUP
 				
 				session::NSLookup lm = nsm.lookup();
-				LOGF("LOOKUP: %s", lm.name().c_str());
+				DBGF("LOOKUP: %s", lm.name().c_str());
 
 				session::NSLookupReply *reply = nsm.mutable_reply();
 				reply->set_ip(name_to_ip[lm.name()]); // TODO: handle name not found
