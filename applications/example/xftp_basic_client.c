433492e6053d0bc24f2137f7a8f5bdb8cb833afd
drbarrett <barrettd@cs.cmu.edu>
2014-05-20 14:20:16 -0700

    Modifed chunk demo code to rerequest dropped CIDs.

--- applications/example/xftp_basic_client.c
+++ applications/example/xftp_basic_client.c
@@ -30,6 +30,7 @@
 #define TITLE "XIA Basic FTP client"
 #define NAME "www_s.basicftp.aaa.xia"
 #define CHUNKSIZE 1024
+#define REREQUEST 3
 
 #define NUM_CHUNKS	10
 
@@ -335,15 +336,22 @@ int getListedChunks(int csock, FILE *fd, char *chunks, char *p_ad, char *p_hid)
 	
 	n = buildChunkDAGs(cs, chunks, p_ad, p_hid);
 	
-	// bring the list of chunks local
-	say("requesting list of %d chunks\n", n);
-	if (XrequestChunks(csock, cs, n) < 0) {
-		say("unable to request chunks\n");
-		return -1;
-	}
-	
-	say("checking chunk status\n");
+	// NOTE: the chunk transport is not currently reliable and chunks may need to be re-requested
+	// ask for the current chunk list again every REREQUEST seconds
+	// chunks already in the local cache will not be refetched from the network 
+	unsigned ctr = 0;
 	while (1) {
+		if (ctr % REREQUEST == 0) {
+			// bring the list of chunks local
+			say("%srequesting list of %d chunks\n", (ctr == 0 ? "" : "re-"), n);
+			if (XrequestChunks(csock, cs, n) < 0) {
+				say("unable to request chunks\n");
+				return -1;
+			}
+			say("checking chunk status\n");
+			ctr++;
+		}
+
 		status = XgetChunkStatuses(csock, cs, n);
 
 		if (status == READY_TO_READ)
