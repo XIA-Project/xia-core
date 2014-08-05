43dc11a7145e37fae167b30941b972c5bebe3f48
Nitin Gupta <nitingupta@cmu.edu>
2014-08-05 10:20:51 -0400

    Removed confusing invalid comment.

--- click-2.0.1/elements/xia/xtransport.hh
+++ click-2.0.1/elements/xia/xtransport.hh
@@ -140,7 +140,7 @@ class XTRANSPORT : public Element {
     bool isAcceptSocket;
     bool initialized;
     bool full_src_dag; // bind to full dag or just to SID  
-    int sock_type; // 0: Reliable transport (SID), 1: Unreliable transport (SID), 2: Content Chunk transport (CID)
+    int sock_type;
     int num_connect_tries; // number of xconnect tries (Xconnect will fail after MAX_CONNECT_TRIES trials)
     int num_migrate_tries; // number of migrate tries (Connection closes after MAX_MIGRATE_TRIES trials)
     int num_retransmit_tries; // number of times to try resending data packets
