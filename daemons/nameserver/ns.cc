95a4e2e627fdac2fc259da5776c831b78b01353f
Nitin Gupta <nitingupta@cmu.edu>
2014-10-15 14:10:25 -0400

    Disable updating SID intent DAGs in nameserver
    When a host moves, the current nameserver notices the change and walks
    through its list of registered services and updates the SID intent DAGs
    for each of them for the moved host. We will disable this behavior
    because the rendezvous server will provide the necessary migration.

--- daemons/nameserver/ns.cc
+++ daemons/nameserver/ns.cc
@@ -170,6 +170,7 @@ void migrate(const char *name, const char *dag)
 				name_to_dag_db_table[name] = dag;
 
 				// now walk the list of registered dags and update those that should migrate
+				/*
 				for (it = name_to_dag_db_table.begin(); it != name_to_dag_db_table.end(); it++) {
 					string ds = it->second;
 					string nm = it->first;
@@ -194,6 +195,7 @@ void migrate(const char *name, const char *dag)
 						name_to_dag_db_table[nm] = g.dag_string();
 					}
 				}
+				*/
 			}
 		}
 
