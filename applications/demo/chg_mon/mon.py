c0aa506e07de280a4503b7462d6d7a5299b60b08
Eric Anderson <andersoe@cs.cmu.edu>
2014-10-28 15:49:26 -0400

    Factor out (currently unused) GUI attempt crud

--- applications/demo/chg_mon/mon.py
+++ applications/demo/chg_mon/mon.py
@@ -5,21 +5,9 @@ import reporting_pb2
 import os
 import pygraphviz
 import networkx as nx
-from threading import Thread
-from time import sleep
 
-import wx
 import matplotlib
-matplotlib.use('WXAgg')
 import matplotlib.pyplot as plt
-
-from traitsui.wx.editor import Editor
-from traits.api import *
-from traitsui.api import View, Item
-from traitsui.wx.basic_editor_factory import BasicEditorFactory
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
-
 
 # find the path to xia-core
 XIADIR=os.getcwd()
@@ -124,65 +112,6 @@ class QuickDag:
         
         return (dg, pos, labels)
 
-def tests():
-    str1 = "RE AD:3d6a88606ef8d04ec4b8a5983a570083bace5690 HID:91b536b0c86a91c15cd614ba4adf132387774df4 SID:785fb7e281640ad1a4fc6d6349eac538be395344"
-    g = dagaddr.Graph(str1)
-    print(g)
-    str2 = g.dag_string()
-    g.print_graph()
-    print(str2)
-    q = QuickDag(str2)
-    print(q)
-    dg, pos, labels = q.toNxDiGraph()
-    print dg.number_of_edges()
-    nx.draw(dg, pos, with_labels=False, node_size=1500, node_color='w')
-    nx.draw_networkx_labels(dg, pos, labels)
-    plt.show()
-    return 
-
-def showdag(str1, intent, whoami, debug=True):
-    fig = plt.figure()
-    ## Normalize to DAG text format (not RE format)
-    str1 = dagaddr.Graph(str1).dag_string()
-    if debug:
-        sys.stderr.write("Processed DAG to %s\n" % str1)
-    q = QuickDag(str1)
-    if debug:
-        sys.stderr.write("Generated QuickDag: %s\n" % str(q))
-    dg, pos, labels = q.toNxDiGraph()
-    nx.draw(dg, pos, with_labels=False, node_size=1500, node_color='w',
-            label="DAG for %s as seen by %s" % (intent, whoami))
-    nx.draw_networkx_labels(dg, pos, labels)
-    plt.savefig("%s_from_%s.png"%(intent,whoami))
-    #return fig
-    
-class _MPLFigureEditor(Editor):
-    scrollable = True
-    
-    def init(self, parent):
-        self.control = self._create_canvas(parent)
-        self.set_tooltip()
-    
-    def update_editor(self):
-        pass
-    
-    def _create_canvas(self, parent):
-        """ Create the MPL canvas."""
-        panel= wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        panel.SetSizer(sizer)
-
-        mpl_control = FigureCanvas(panel, -1, self.value)
-        sizer.Add(mpl_control, 1, wx.LEFT | wx.TOP | wx.GROW)
-        toolbar = NavigationToolbar2Wx(mpl_control)
-        sizer.Add(toolbar, 0, wx.EXPAND)
-        self.value.canvas.SetMinSize((10,10))
-        return panel
-
-
-class MPLFigureEditor(BasicEditorFactory):
-    klass = _MPLFigureEditor
-
 def watch(socket):
     socket.connect("tcp://%s:%s" % (SERVER, PORT))
     socket.setsockopt(zmq.SUBSCRIBE, "/dagchange/")
@@ -200,67 +129,39 @@ def watch(socket):
     return 0
     
 
-class SocketWatcherThread(Thread):
-    """Listens for ZMQ/Protobuf messages"""
-    wants_abort = False
 
-    def run(self):
-        """Wait and do things"""
-        self.socket.connect("tcp://%s:%s" % (SERVER, PORT))
-        self.socket.setsockopt(zmq.SUBSCRIBE, "/dagchange/")
-        pb_msg = reporting_pb2.AddrChange()
+def tests():
+    str1 = "RE AD:3d6a88606ef8d04ec4b8a5983a570083bace5690 HID:91b536b0c86a91c15cd614ba4adf132387774df4 SID:785fb7e281640ad1a4fc6d6349eac538be395344"
+    g = dagaddr.Graph(str1)
+    print(g)
+    str2 = g.dag_string()
+    g.print_graph()
+    print(str2)
+    q = QuickDag(str2)
+    print(q)
+    dg, pos, labels = q.toNxDiGraph()
+    print dg.number_of_edges()
+    nx.draw(dg, pos, with_labels=False, node_size=1500, node_color='w')
+    nx.draw_networkx_labels(dg, pos, labels)
+    plt.show()
+    return 
 
-        print "running"
-        while not self.wants_abort:
-            header,msg = self.socket.recv_multipart()
-            print(header)
-            pb_msg.ParseFromString(msg)
-            print(pb_msg)
-            self.display(str(pb_msg.newdag))
-            self.dag_show(str(pb_msg.newdag))
-
-class Test (HasTraits):
-    figure = Instance(matplotlib.figure.Figure, ())
-    socket_watcher_thread = Instance(SocketWatcherThread)
-    results_string = String()
-
-    view = View(Item('figure', editor=MPLFigureEditor(), show_label=False),
-                Item('results_string', show_label = False, springy=True),
-            width=400,
-                height=300,
-                resizable=True)
+def showdag(str1, intent, whoami, debug=False):
+    fig = plt.figure()
+    ## Normalize to DAG text format (not RE format)
+    str1 = dagaddr.Graph(str1).dag_string()
+    if debug:
+        sys.stderr.write("Processed DAG to %s\n" % str1)
+    q = QuickDag(str1)
+    if debug:
+        sys.stderr.write("Generated QuickDag: %s\n" % str(q))
+    dg, pos, labels = q.toNxDiGraph()
+    nx.draw(dg, pos, with_labels=False, node_size=1500, node_color='w',
+            label="DAG for %s as seen by %s" % (intent, whoami))
+    nx.draw_networkx_labels(dg, pos, labels)
+    plt.savefig("%s_from_%s.png"%(intent,whoami))
+    #return fig
     
-        
-    def _figure_default(self):
-        figure = matplotlib.figure.Figure()
-        return figure
-            
-    def __init__(self):
-        super(Test, self).__init__()
-        context = zmq.Context()
-        socket = context.socket(zmq.SUB)
-
-        axes = self.figure.add_subplot(111)
-            
-        #Start socket watcher
-        if self.socket_watcher_thread and self.socket_watcher_thread.isAlive():
-            pass
-        else:
-            self.socket_watcher_thread = SocketWatcherThread()
-            self.socket_watcher_thread.socket = socket
-            self.socket_watcher_thread.display = self.add_line
-            self.socket_watcher_thread.dag_show = self.dag_show
-            self.socket_watcher_thread.ax = axes
-            self.socket_watcher_thread.start()
-                
-        def add_line(self, string):
-            self.results_string = self.results_string + "\n" + string
-
-        def dag_show(self, dagstr):
-            showdag(dagstr, self.figure.axes)
-            wx.CallAfter(self.figure.canvas.draw)
-            return
-
 
 def main(args):
     ## Init ZMQ
