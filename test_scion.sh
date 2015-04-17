7c0db9c8d49d939816d48c998e603140ba1f895c
tenma <tenma.lin@gmail.com>
2015-04-17 13:20:12 -0400

    remove unused logs file.

--- test_scion.sh
+++ test_scion.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ./bin/xianet kill
 ./configure
-make -j3
+make -j4
+make -j4
 #./bin/xianet -s xia_scion_topology.click -V -l 7 start
 ./bin/xianet -s xia_scion_topology.click -V start
