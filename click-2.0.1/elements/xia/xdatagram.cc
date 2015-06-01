c84681aaf348a498913a44cafa084ac0bce930db
Tianyuan Ding <dingty@163.com>
2015-05-31 23:54:51 -0400

    Add raw files

---
+++ click-2.0.1/elements/xia/xdatagram.cc
@@ -0,0 +1,97 @@
+#include "../../userlevel/xia.pb.h"
+#include <click/config.h>
+#include <click/glue.hh>
+#include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/packet_anno.hh>
+#include <click/packet.hh>
+#include <click/vector.hh>
+
+#include <click/xiacontentheader.hh>
+#include "xdatagram.hh"
+#include "xtransport.hh"
+
+#include <click/xiatransportheader.hh>
+
+
+CLICK_DECLS
+
+XDatagram::XDatagram(XTRANSPORT *transport, unsigned short port)
+	: XGenericTransport(transport, port, XSOCKET_DGRAM) {}
+
+void
+XDatagram::push(WritablePacket *p_in) {
+		// buffer packet if this is a DGRAM socket and we have room
+	if (should_buffer_received_packet(p_in)) {
+		add_packet_to_recv_buf(p_in);
+		if (polling) {
+			// tell API we are readable
+			get_transport()->ProcessPollEvent(port, POLLIN);
+		}
+		check_for_and_handle_pending_recv();
+	}
+}
+
+bool 
+XDatagram::should_buffer_received_packet(WritablePacket *p) {
+	if (recv_buffer_count < recv_buffer_size) return true;
+	else return false;
+}
+
+void 
+XDatagram::add_packet_to_recv_buf(WritablePacket *p) {
+	int index = (dgram_buffer_end + 1) % recv_buffer_size;
+	dgram_buffer_end = index;
+	recv_buffer_count++;
+	WritablePacket *p_cpy = p->clone()->uniqueify();
+	recv_buffer[index] = p_cpy;
+}
+
+void
+XDatagram::check_for_and_handle_pending_recv() {
+	if (recv_pending) {
+		int bytes_returned = read_from_recv_buf(pending_recv_msg);
+		get_transport()->ReturnResult(port, pending_recv_msg, bytes_returned);
+
+		recv_pending = false;
+		delete pending_recv_msg;
+		pending_recv_msg = NULL;
+	}
+}
+
+int
+XDatagram::read_from_recv_buf(XSocketMsg *xia_socket_msg) {
+	X_Recvfrom_Msg *x_recvfrom_msg = xia_socket_msg->mutable_x_recvfrom();
+
+	// Get just the next packet in the recv buffer (we don't return data from more
+	// than one packet in case the packets came from different senders). If no
+	// packet is available, we indicate to the app that we returned 0 bytes.
+	WritablePacket *p = recv_buffer[dgram_buffer_start];
+
+	if (recv_buffer_count > 0 && p) {
+		XIAHeader xiah(p->xia_header());
+		TransportHeader thdr(p);
+		int data_size = xiah.plen() - thdr.hlen();
+
+		String src_path = xiah.src_path().unparse();
+		String payload((const char*)thdr.payload(), data_size);
+		x_recvfrom_msg->set_payload(payload.c_str(), payload.length());
+		x_recvfrom_msg->set_sender_dag(src_path.c_str());
+		x_recvfrom_msg->set_bytes_returned(data_size);
+
+		p->kill();
+		recv_buffer[dgram_buffer_start] = NULL;
+		recv_buffer_count--;
+		dgram_buffer_start = (dgram_buffer_start + 1) % recv_buffer_size;
+		return data_size;
+	} else {
+		x_recvfrom_msg->set_bytes_returned(0);
+		return 0;
+	}
+}
+
+CLICK_ENDDECLS
+
+EXPORT_ELEMENT(XDatagram)
+ELEMENT_REQUIRES(userlevel)
+ELEMENT_REQUIRES(XIAContentModule)
---
+++ click-2.0.1/elements/xia/xdatagram.hh
@@ -0,0 +1,78 @@
+#ifndef CLICK_XDATAGRAM_HH
+#define CLICK_XDATAGRAM_HH
+
+#include <click/element.hh>
+#include <clicknet/xia.h>
+#include <click/xid.hh>
+#include <click/xiaheader.hh>
+#include <click/hashtable.hh>
+#include "xiaxidroutetable.hh"
+#include <click/handlercall.hh>
+#include <click/xiapath.hh>
+#include <clicknet/xia.h>
+#include "xiacontentmodule.hh"
+#include "xiaxidroutetable.hh"
+#include <clicknet/udp.h>
+#include <click/string.hh>
+#include <elements/ipsec/sha1_impl.hh>
+#include <click/xiatransportheader.hh>
+#include "xtransport.hh"
+
+#if CLICK_USERLEVEL
+#include <list>
+#include <stdio.h>
+#include <iostream>
+#include <click/xidpair.hh>
+#include <click/timer.hh>
+#include <click/packet.hh>
+#include <queue>
+#include "../../userlevel/xia.pb.h"
+
+using namespace std;
+using namespace xia;
+#endif
+
+// FIXME: put these in a std location that can be found by click and the API
+#define XOPT_HLIM 0x07001
+#define XOPT_NEXT_PROTO 0x07002
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+
+#define UNUSED(x) ((void)(x))
+
+CLICK_DECLS
+
+
+class XIAContentModule;   
+
+class XDatagram : public XGenericTransport {
+
+public:
+	XDatagram(XTRANSPORT *transport, unsigned short port);
+	XDatagram(){};
+	~XDatagram() {};
+	int read_from_recv_buf(XSocketMsg *xia_socket_msg);
+private:
+
+	void push(WritablePacket *p_in);
+	bool should_buffer_received_packet(WritablePacket *p);
+	void add_packet_to_recv_buf(WritablePacket *p);
+	void check_for_and_handle_pending_recv();
+
+	// receive buffer
+	WritablePacket *recv_buffer[MAX_RECV_WIN_SIZE]; // packets we've received but haven't delivered to the app // TODO: start smaller, dynamically resize if app asks for more space (up to MAX)?
+	uint32_t recv_buffer_size; // the number of PACKETS we can buffer (received but not delivered to app)
+	uint32_t recv_base; // sequence # of the oldest received packet not delivered to app
+	uint32_t next_recv_seqnum; // the sequence # of the next in-order packet we expect to receive
+	int dgram_buffer_start; // the first undelivered index in the recv buffer (DGRAM only)
+	int dgram_buffer_end; // the last undelivered index in the recv buffer (DGRAM only)
+	uint32_t recv_buffer_count; // the number of packets in the buffer (DGRAM only)
+
+} ;
+
+CLICK_ENDDECLS
+
+#endif
