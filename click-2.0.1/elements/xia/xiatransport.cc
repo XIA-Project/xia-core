4de97483b1b2a11a67463875a7b3057dd745ee21
lendlice <chenren.xu@gmail.com>
2015-06-16 15:43:39 -0400

    add comments to cache related xia click elements

--- click-2.0.1/elements/xia/xiatransport.cc
+++ click-2.0.1/elements/xia/xiatransport.cc
@@ -15,57 +15,54 @@
 CLICK_DECLS
 XIATransport::XIATransport()
 {
-    cp_xid_type("CID", &_cid_type);   
-    // oldPartial=contentTable;
-    _content_module = new XIAContentModule(this);
+	cp_xid_type("CID", &_cid_type);   
+	// oldPartial=contentTable;
+	_content_module = new XIAContentModule(this);
 }
 
 XIATransport::~XIATransport()
 {
-    delete _content_module;
+	delete _content_module;
 }
 
-int 
-XIATransport::configure(Vector<String> &conf, ErrorHandler *errh)
+int XIATransport::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-    Element* routing_table_elem;
-    XIAPath local_addr;
-    bool cache_content_from_network =true;
+	Element* routing_table_elem;
+	XIAPath local_addr;
+	bool cache_content_from_network =true;
 
-    if (cp_va_kparse(conf, this, errh,
-		"LOCAL_ADDR", cpkP+cpkM, cpXIAPath, &local_addr,
-		"ROUTETABLENAME", cpkP+cpkM, cpElement, &routing_table_elem,
-		"CACHE_CONTENT_FROM_NETWORK", cpkP, cpBool, &cache_content_from_network,
-		cpEnd) < 0)
-	return -1;   
+	if (cp_va_kparse(conf, this, errh,
+			"LOCAL_ADDR", cpkP+cpkM, cpXIAPath, &local_addr,
+			"ROUTETABLENAME", cpkP+cpkM, cpElement, &routing_table_elem,
+			"CACHE_CONTENT_FROM_NETWORK", cpkP, cpBool, &cache_content_from_network, cpEnd) < 0)
+	return -1;
+
 #if USERLEVEL
-    _content_module->_routeTable = dynamic_cast<XIAXIDRouteTable*>(routing_table_elem);
+	_content_module->_routeTable = dynamic_cast<XIAXIDRouteTable*>(routing_table_elem);
 #else
-    _content_module->_routeTable = reinterpret_cast<XIAXIDRouteTable*>(routing_table_elem);
+ 	_content_module->_routeTable = reinterpret_cast<XIAXIDRouteTable*>(routing_table_elem);
 #endif
-    _local_addr = local_addr;
-    _local_hid = local_addr.xid(local_addr.destination_node());
+	_local_addr = local_addr;
+	_local_hid = local_addr.xid(local_addr.destination_node());
 
-    _content_module->_cache_content_from_network = true;
-    /*
-       std::cout<<"Route Table Name: "<<routing_table_name.c_str()<<std::endl;
-       if(routeTable==NULL) 
-            std::cout<<"NULL routeTable"<<std::endl;
-       if(ad_given)
-       {
-           std::cout<<"ad: "<<sad.c_str()<<std::endl;
-           ad.parse(sad);
-           std::cout<<ad.unparse().c_str()<<std::endl;
-       }
-       if(hid_given)
-       {
-           std::cout<<"hid: "<<shid.c_str()<<std::endl;
-           hid.parse(shid);
-           std::cout<<hid.unparse().c_str()<<std::endl;
-       }
-       std::cout<<"pkt size: "<<PKTSIZE<<std::endl;
-     */
-    return 0;
+	_content_module->_cache_content_from_network = true;
+	/*
+		std::cout<<"Route Table Name: "<<routing_table_name.c_str()<<std::endl;
+		if(routeTable==NULL) 
+			std::cout<<"NULL routeTable"<<std::endl;
+		if(ad_given) {
+			std::cout<<"ad: "<<sad.c_str()<<std::endl;
+			ad.parse(sad);
+			std::cout<<ad.unparse().c_str()<<std::endl;
+		}
+		if(hid_given) {
+			std::cout<<"hid: "<<shid.c_str()<<std::endl;
+			hid.parse(shid);
+			std::cout<<hid.unparse().c_str()<<std::endl;
+		}
+		std::cout<<"pkt size: "<<PKTSIZE<<std::endl;
+	*/
+	return 0;
 }
 
 //TODO: remove
@@ -83,34 +80,32 @@ static void say(const char *fmt, ...)
 void XIATransport::push(int port, Packet *p)
 {
 	//TODO:remove
-    printf(">>>>in XIATRANSPORT PUSH");
+	printf(">>>>in XIATRANSPORT PUSH");
 
-    const struct click_xia* hdr = p->xia_header();
+	const struct click_xia* hdr = p->xia_header();
 
-    if (!hdr) return;
-    if (hdr->dnode == 0 || hdr->snode == 0) return;
+	if (!hdr) return;
+	if (hdr->dnode == 0 || hdr->snode == 0) return;
 
-    // Parse XIA source and destination
-    struct click_xia_xid __dstID =  hdr->node[hdr->dnode - 1].xid;
-    uint32_t dst_xid_type = __dstID.type;
-    struct click_xia_xid __srcID = hdr->node[hdr->dnode + hdr->snode - 1].xid;
-    uint32_t src_xid_type = __srcID.type;
+	// Parse XIA source and destination
+	struct click_xia_xid __dstID =  hdr->node[hdr->dnode - 1].xid;
+	uint32_t dst_xid_type = __dstID.type;
+	struct click_xia_xid __srcID = hdr->node[hdr->dnode + hdr->snode - 1].xid;
+	uint32_t src_xid_type = __srcID.type;
 
-    XID dstID(__dstID);
-    XID srcID(__srcID);
+	XID dstID(__dstID);
+	XID srcID(__srcID);
 
-
-    if(src_xid_type==_cid_type)  //store, this is chunk response
-   	_content_module->cache_incoming(p, srcID, dstID, port);
-    else if(dst_xid_type==_cid_type)  //look_up,  chunk request
-   	_content_module->process_request(p, srcID, dstID);
-    else
-    {
-	p->kill();
+	if (src_xid_type == _cid_type)  // store, this is chunk response
+		_content_module->cache_incoming(p, srcID, dstID, port);
+	else if(dst_xid_type==_cid_type)  // look_up,  chunk request
+		_content_module->process_request(p, srcID, dstID);
+	else {
+		p->kill();
 #if DEBUG_PACKET
 	click_chatter("src and dst are not CID");
 #endif
-    }
+	}
 }
 
 
