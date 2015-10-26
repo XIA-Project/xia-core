fbcbb87aaddff070c06c25f73fed72eff730690b
Nitin Gupta <nitingupta@cmu.edu>
2015-10-26 14:57:32 -0400

    Xnetjd implementation - first pass
    This will change substantially after the network joining pieces fall
    into place.

--- daemons/xnetjd/xnetjd.cc
+++ daemons/xnetjd/xnetjd.cc
@@ -1,11 +1,20 @@
 #include "Xsocket.h"
+#include "xnetjd.h"
+#include <map>
+#include <vector>
 #include <iostream>
+#include <netinet/ether.h>
 
 using namespace std;
 
-#define XIANETJOIN_API_PORT 9882
 #define MAXBUFSIZE 2048
 #define MACADDRSTRLEN 18
+#define LOCAL_HEADER_SIZE sizeof(uint16_t)+sizeof(struct ether_addr)
+
+// TODO: Convert to non-global
+int netjsock;
+std::map<NetJoinMsgType, std::vector<struct sockaddr_in> > msg_subscribers;
+std::map<NetJoinMsgType, std::vector<struct sockaddr_in> >::iterator msg_sub_iter;
 
 class NetjoinConfig {
 	private:
@@ -24,8 +33,8 @@ class NetjoinLocalHeader {
 	private:
 		int _headersize;
 		int _interfaceID;
-		char _sender_mac_addr[6];
-		char _sender_mac_addr_str[18];
+		char _sender_mac_addr[6];      // TODO: Use struct ether_addr
+		char _sender_mac_addr_str[MACADDRSTRLEN]; // TODO: Use ether_ntoa() instead.
 	public:
 		int interface() {return _interfaceID;}
 		char *sender_str() {return _sender_mac_addr_str;}
@@ -44,11 +53,102 @@ class NetjoinLocalHeader {
 		}
 };
 
-int handle_message(char *msg, int msgsize)
+int handle_subscription(char *msg, int msgsize,
+		struct sockaddr_in sender, socklen_t senderlen)
 {
+	if(senderlen != sizeof(struct sockaddr_in)) {
+		cout << "Sender's address is not a sockaddr_in" << endl;
+		return -1;
+	}
+	// NOTE: In future, we can add subscription verification here
+	// Retrieve msgtype
+	NetJoinMsgType type = static_cast<NetJoinMsgType>((uint8_t) *msg);
+	if(msgsize > 2) {
+		cout << "Subscription request has something more than msg type" << endl;
+		return -1;
+	}
+	// Add msgtype and sender to table
+	msg_subscribers[type].push_back(sender);
+	return 0;
+}
+
+int send_message(sockaddr_in dest, char *msg, int msgsize)
+{
+	int rc;
+	int retval = 0;
+	rc = sendto(netjsock, msg, msgsize, 0, (struct sockaddr*)&dest, sizeof(struct sockaddr_in));
+	if(rc < msgsize) {
+		cout << "Unable to send message" << endl;
+		retval = -1;
+	}
+	return retval;
+}
+
+// Convert wire message type to WIRE_UNDERLYING_TYPE
+// NOTE: INFINITE RECURSION IF TYPE NOT CHANGED AND SUCCESS(0) RETURNED
+int handle_wire_message(char *msg, int msgsize)
+{
+	// Wire messages have a local header, extract it
 	NetjoinLocalHeader header(msg, msgsize);
 	cout << "Received pkt on interface" << header.interface();
 	cout << "From " << header.sender_str() << endl;
+	NetJoinMsgType type = static_cast<NetJoinMsgType>((uint8_t) *(msg+LOCAL_HEADER_SIZE));
+	if(type == XHCP_BEACON) {
+		*msg = (uint8_t) WIRE_XHCP_BEACON;
+	} else {
+		return -1;
+	}
+	return 0;
+}
+
+int handle_message(char *msg, int msgsize,
+		struct sockaddr_in sender, socklen_t senderlen)
+{
+	NetJoinMsgType type = static_cast<NetJoinMsgType>((uint8_t) *msg);
+	msg++;
+	msgsize--;
+	switch(type) {
+		case SUBSCRIBE:
+			// update msg_subscribers table
+			handle_subscription(msg, msgsize, sender, senderlen);
+			break;
+		case WIRE_PACKET:
+			// convert message type to wire version of underlying message
+			if(handle_wire_message(msg, msgsize)) {
+				cout << "Unable to handle wire message" << endl;
+				return -1;
+			}
+			// re-process message
+			handle_message(msg, msgsize, sender, senderlen);
+			break;
+		case XHCP_BEACON:
+			// Deliver to beacon subscribers - xianetjoin (hardcoded for now)
+			cout << "Got XHCP Beacon from an application" << endl;
+			msg_sub_iter = msg_subscribers.find(XHCP_BEACON);
+			if(msg_sub_iter != msg_subscribers.end()) {
+				std::vector<struct sockaddr_in> subscribers = msg_subscribers[XHCP_BEACON];
+				std::vector<struct sockaddr_in>::iterator it;
+				for(it=subscribers.begin();it!=subscribers.end();it++) {
+					// TODO: Send msg to the subscriber
+					send_message(*it, msg, msgsize);
+					cout << "Forwarded XHCP Beacon (most likely to Click)" << endl;
+				}
+			}
+		case WIRE_XHCP_BEACON:
+			// Forward to subscribers of WIRE_XHCP_BEACON
+			cout << "Forwarding Wire XHCP Beacon" << endl;
+			msg_sub_iter = msg_subscribers.find(WIRE_XHCP_BEACON);
+			if(msg_sub_iter != msg_subscribers.end()) {
+				std::vector<struct sockaddr_in> subscribers = msg_subscribers[WIRE_XHCP_BEACON];
+				std::vector<struct sockaddr_in>::iterator it;
+				for(it=subscribers.begin();it!=subscribers.end();it++) {
+					// TODO: Send msg to the subscriber
+					send_message(*it, msg, msgsize);
+					cout << "Forwarded Wire XHCP Beacon" << endl;
+				}
+			}
+			break;
+	};
 
 	// Read in the protobuf containing netjoin message type and message here
 	return 0;
@@ -78,11 +178,10 @@ int bound_socket(NetjoinConfig *config)
 
 int main_loop(NetjoinConfig *config)
 {
-	int netjsock;
 	int msgsize;
 	char buf[MAXBUFSIZE];
-	struct sockaddr_in xianetjoinaddr;
-	socklen_t xianetjoinaddrlen = sizeof(xianetjoinaddr);
+	struct sockaddr_in senderaddr;
+	socklen_t senderaddrlen = sizeof(senderaddr);
 
 	netjsock = bound_socket(config);
 	if(netjsock < 0) {
@@ -93,12 +192,12 @@ int main_loop(NetjoinConfig *config)
 	while (true) {
 		bzero(buf, sizeof(buf));
 		msgsize = recvfrom(netjsock, buf, config->maxbufsize(), 0,
-				(struct sockaddr *)&xianetjoinaddr, &xianetjoinaddrlen);
+				(struct sockaddr *)&senderaddr, &senderaddrlen);
 		if(msgsize < 0) {
 			cout << "Failed receiving a packet, skipping..." << endl;
 		}
 
-		if(handle_message(buf, msgsize)) {
+		if(handle_message(buf, msgsize, senderaddr, senderaddrlen)) {
 			cout << "Failed handling message, skipping..." << endl;
 		}
 	}
@@ -112,19 +211,37 @@ NetjoinConfig *parse_options(int argc, char **argv)
 		return NULL;
 	}
 	cout << argv[1] << " Started" << endl;
+	// TODO: Pass option to start forwarding XHCP_Beacons
+	// for now, just add xianetjoin element as a subscriber of XHCP_Beacons
+	struct sockaddr_in xianetjoinaddr;
+	xianetjoinaddr.sin_family = AF_INET;
+	xianetjoinaddr.sin_addr.s_addr = INADDR_ANY;
+	xianetjoinaddr.sin_port = htons(XIANETJOIN_ELEMENT_PORT);
+	msg_subscribers[XHCP_BEACON].push_back(xianetjoinaddr);
 	return new NetjoinConfig();
 }
 
 int main(int argc, char **argv)
 {
+	int state = 0;
+	int retval = 0;
+
 	NetjoinConfig *config = parse_options(argc, argv);
 	if(config == NULL) {
 		cout << "Failed parsing options" << endl;
-		return -1;
+		retval = -1;
+		goto main_done;
 	}
+	state = 1;
 	if(main_loop(config)) {
 		cout << "Error: main loop ended" << endl;
-		return -1;
+		retval = -1;
+		goto main_done;
 	}
-	return 0;
+main_done:
+	switch(state) {
+		case 1:
+			delete config;
+	}
+	return retval;
 }
