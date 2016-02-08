7ef8f27ec92e71c959b2e5baaf8bfccfd7183e0c
drbarrett <barrettd@cs.cmu.edu>
2016-02-08 17:12:09 -0500

    Fixed coredump when packetsize is random.

--- applications/example/echoclient.c
+++ applications/example/echoclient.c
@@ -36,6 +36,8 @@
 
 #define STREAM_NAME "www_s.stream_echo.aaa.xia"
 
+#define MAX_BUF_SIZE 0x80000 // Limit test packets to 512k
+
 
 // FIXME: clean up globals and move into a structure or similar
 // global configuration options
@@ -105,7 +107,7 @@ void getConfig(int argc, char** argv)
 				// if 0, send random sized packets
 				pktSize = atoi(optarg);
 				if (pktSize < 0) pktSize = 0;
-				// if (pktSize > XIA_MAXBUF) pktSize = XIA_MAXBUF;
+				 if (pktSize > MAX_BUF_SIZE) pktSize = MAX_BUF_SIZE;
 				break;
 			case 'r':
 				// close and reopen the connection every <reconnect> operations
@@ -201,10 +203,10 @@ int process(int sock)
 	int sent = 0;
 	int received = 0;
 	int rc = 0;
-	char buf1[pktSize + 1], buf2[pktSize + 1], temp[pktSize + 1];
+	char buf1[MAX_BUF_SIZE + 1], buf2[MAX_BUF_SIZE + 1], temp[MAX_BUF_SIZE + 1];
 	char *bufp = buf1;
 	if (pktSize == 0)
-		size = (rand() % pktSize) + 1;
+		size = (rand() % MAX_BUF_SIZE) + 1;
 	else
 		size = pktSize;
 	randomString(buf1, size);
