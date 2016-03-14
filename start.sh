64bbf5697af95359671ee6e902d37d7d5c503c80
harshadjs <harshadshirwadkar@gmail.edu>
2016-03-14 19:46:04 -0400

    Cleaned up

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 rm etc/click/templates/host.click
 rm etc/click/templages/router.click
+sudo rm -rf /tmp/content/*
 if [ "$1" = "host" ]; then
-   sudo ./bin/xianet -t -I eth4 ${2}
+   sudo ./bin/xianet -t -I eth1 ${2}
 fi
 if [ "$1" = "router" ]; then
    sudo ./bin/xianet -nr ${2}
