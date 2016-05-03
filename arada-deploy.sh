240fce7103e6015b0199b31ee2f4eed640f42bda
Nitin Gupta <nitingupta@cmu.edu>
2016-05-03 12:05:59 -0400

    Simplified Arada OBU/RSU deployment steps.
    This scripts now assumes that the arada-install.sh script has been
    successfully run and it has prepared the necessary packages for going on
    to the Arada hardware. We were not saving much with preparing a tarball
    and unpacking it, so I decided to switch to a simple recursive scp
    instead. That also ensures that the user is asked for the password once
    instead of twice.

--- arada-deploy.sh
+++ arada-deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 
-BUILDROOT=/opt/buildroot-2013.11/output/host
+BUILDROOT=/opt/buildroot-2013.11/output/host/usr
 SANDBOX=arada_deploy_sandbox
 XIADIR=xia-core
 
-BUILDROOTBIN=$BUILDROOT/usr/bin
-BUILDROOTLIB=$BUILDROOT/usr/lib
+BUILDROOTBIN=$BUILDROOT/bin
+BUILDROOTLIB=$BUILDROOT/lib
+BUILDROOTXIA=$BUILDROOT/$XIADIR
 SANDBOXBIN=$SANDBOX/bin
 SANDBOXLIB=$SANDBOX/lib
-SANDBOXXIAAPILIB=$SANDBOX/$XIADIR/api/lib
-SANDBOXXIABIN=$SANDBOX/$XIADIR/bin
+SANDBOXXIA=$SANDBOX/$XIADIR
 
 TARBALL=${SANDBOX}.tar.gz
 
@@ -37,15 +37,37 @@ fi
 echo "Preparing files to be sent to target"
 rm -fr $SANDBOX
 mkdir $SANDBOX
+if [ $? -ne 0 ]; then
+	echo "Failed creating $SANDBOX"
+	exit -1
+fi
 mkdir -p $SANDBOXBIN
+if [ $? -ne 0 ]; then
+	echo "Failed creating$SANDBOXBIN"
+	exit -1
+fi
 mkdir -p $SANDBOXLIB
-mkdir -p $SANDBOXXIAAPILIB
-mkdir -p $SANDBOXXIABIN
-cp $BUILDROOTBIN/python* $SANDBOXBIN
+if [ $? -ne 0 ]; then
+	echo "Failed creating $SANDBOXLIB"
+	exit -1
+fi
+mkdir -p $SANDBOXXIA
+if [ $? -ne 0 ]; then
+	echo "Failed creating $SANDBOXXIA"
+	exit -1
+fi
+
+pushd $BUILDROOTBIN > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Failed to switch to $BUILDROOTBIN"
+	exit -1
+fi
+cp python* openssl tr bash $SANDBOXBIN
 if [ $? -ne 0 ]; then
 	echo "Failed copying binaries from $BUILDROOTBBIN"
 	exit -5
 fi
+popd # $BUILDROOTBIN
 
 cp -R $BUILDROOTLIB/* $SANDBOXLIB
 if [ $? -ne 0 ]; then
@@ -53,52 +75,19 @@ if [ $? -ne 0 ]; then
 	exit -6
 fi
 
-cp -R api/lib/* $SANDBOXXIAAPILIB
+cp -R $BUILDROOTXIA/* $SANDBOXXIA
 if [ $? -ne 0 ]; then
-	echo "Failed copying XIA libraries to $SANDBOXXIAAPILIB"
+	echo "Failed copying XIA to $SANDBOXXIA"
 	exit -7
 fi
 
-cp -R bin/* $SANDBOXXIABIN
-if [ $? -ne 0 ]; then
-	echo "Failed copying XIA executables to $SANDBOXXIABIN"
-	exit -8
-fi
-
-
-cp click/userlevel/click $SANDBOXBIN
-if [ $? -ne 0 ]; then
-	echo "Failed copying click executable"
-	exit -9
-fi
-
 cd $SANDBOX
-tar -chzf ../$TARBALL *
+echo "Deploying files on target"
+scp -r * root@$1:/tmp/usb/
 if [ $? -ne 0 ]; then
-	echo "Failed creating tarball for target"
-	exit -10
-fi
-cd .. #sandbox
-
-echo "Copying files to target."
-echo "Provide root@$1 password when prompted."
-sleep 1
-
-scp $TARBALL root@$1:/tmp/usb/
-if [ $? -ne 0 ]; then
-	echo "Failed copying tarball to target"
+	echo "Failed copying everything to target"
 	exit -11
 fi
-
-echo "Deploying files on target"
-ssh root@$1 "cd /tmp/usb; tar -xzf $TARBALL"
-if [ $? -ne 0 ]; then
-	echo "Failed unpacking tarball on target"
-	exit -12
-fi
-
-# Clean up the sandbox
-rm -fr $SANDBOX
-rm $TARBALL
+cd ..
 
 echo "All files successfully deployed on target."
