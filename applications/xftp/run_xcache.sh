dbcefacd0d5d5b1925bf51fe6bb03c728bcdd099
zihaol1 <zihaol1@andrew.cmu.edu>
2016-07-26 16:55:16 -0400

    update run xcache script

--- applications/xftp/run_xcache.sh
+++ applications/xftp/run_xcache.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 kill $(ps aux | grep '/bin/xcache' | awk '{print $2}')
-LD_LIBRARY_PATH=~/xia-core/api/lib ~/xia-core/bin/xcache -h host1 -l3 -m  0xFF&
-
+LD_LIBRARY_PATH=~/xia-core/api/lib ~/xia-core/bin/xcache -h host1 -v -l7&
