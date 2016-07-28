8b8376f7c5b14f946c1f32abdfd80077ed5dd220
Nitin Gupta <nitingupta@cmu.edu>
2016-07-28 12:29:54 -0400

    Use the default interface when setting default routes
    For now, the default interface is always set to 0. Ideally, it should
    change to maybe the first network joined, or as specified by the user.

--- click/elements/xia/xtransport.cc
+++ click/elements/xia/xtransport.cc
@@ -1994,19 +1994,19 @@ void XTRANSPORT::Xupdatedag(unsigned short _sport, uint32_t id, xia::XSocketMsg 
 
 		// Set default AD to point to new RHID
 		cmd = ad_table_str + ".set4";
-		cmdargs = default_AD + ",0," + new_rhid.unparse() + "," + String(0xffff);
+		cmdargs = default_AD + "," + interface + "," + new_rhid.unparse() + "," + String(0xffff);
 		click_chatter("XTRANSPORT: %s ...%s.", cmd.c_str(), cmdargs.c_str());
 		HandlerCall::call_write(cmd.c_str(), cmdargs.c_str(), this);
 
 		// Set default HID to point to new RHID
 		cmd = hid_table_str + ".set4";
-		cmdargs = default_HID + ",0," + new_rhid.unparse() + "," + String(0xffff);
+		cmdargs = default_HID + "," + interface + "," + new_rhid.unparse() + "," + String(0xffff);
 		click_chatter("XTRANSPORT: %s ...%s.", cmd.c_str(), cmdargs.c_str());
 		HandlerCall::call_write(cmd.c_str(), cmdargs.c_str(), this);
 
 		// Set default AD to point to new RHID
 		cmd = ip_table_str + ".set4";
-		cmdargs = default_4ID + ",0," + new_rhid.unparse() + "," + String(0xffff);
+		cmdargs = default_4ID + "," + interface + "," + new_rhid.unparse() + "," + String(0xffff);
 		click_chatter("XTRANSPORT: %s ...%s.", cmd.c_str(), cmdargs.c_str());
 		HandlerCall::call_write(cmd.c_str(), cmdargs.c_str(), this);
 	}
