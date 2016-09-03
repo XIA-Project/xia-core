ff44fa95112b29e5978af9628b0f838555621bcd
zihaol1 <zihaol1@andrew.cmu.edu>
2016-09-02 21:00:17 -0400

    fix get workload script

--- daemons/xcidrouted/bin/getworkload.sh
+++ daemons/xcidrouted/bin/getworkload.sh
@@ -3,4 +3,4 @@
 workload_port=31546
 workload_host=zihaol1@pc4.instageni.stanford.edu
 
-scp -P $workload_port $workload_host:~/xia-core/daemons/xcidrouted/workload/* workload/*
+scp -P $workload_port $workload_host:~/xia-core/daemons/xcidrouted/workload/* workload/.
