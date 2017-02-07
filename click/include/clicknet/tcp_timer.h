950abcb4cee6acec75f6a753d03f16c217ca7a5f
Jing <763118287@qq.com>
2017-02-07 16:59:11 -0500

    Update tcp_timer.h

--- click/include/clicknet/tcp_timer.h
+++ click/include/clicknet/tcp_timer.h
@@ -101,7 +101,7 @@
                                                          //idle time bef. drop */
 #define	TCPTV_MIN       (1*PR_SLOWHZ) //(1*PR_SLOWHZ/100) /* minimum allowable value */
 #define	TCPTV_REXMTMAX	(64*PR_SLOWHZ) /* max allowable REXMT value */
-#define	TCP_MAXRXTSHIFT	30//12  /* maximum retransmits */
+#define	TCP_MAXRXTSHIFT	12  /* maximum retransmits */
 #define	TCP_LINGERTIME	120 /* linger at most 2 minutes UNUSED! */
 
 #ifdef	TCPTIMERS
