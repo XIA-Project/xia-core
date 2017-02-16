abcf610a1bb4c2753cab66534a6777f08cde9d23
Nitin Gupta <nitingupta@cmu.edu>
2017-02-16 12:30:43 -0500

    message indicating start of network announcement

--- daemons/xnetjd/netjoin_session.py
+++ daemons/xnetjd/netjoin_session.py
@@ -12,7 +12,6 @@ import c_xsocket
 from clickcontrol import ClickControl
 from ndap_pb2 import LayerTwoIdentifier, NetDescriptor
 from netjoin_beacon import NetjoinBeacon
-from netjoin_announcer import NetjoinAnnouncer
 from netjoin_message_pb2 import NetjoinMessage
 from netjoin_authsession import NetjoinAuthsession
 from netjoin_dsrc_handler import NetjoinDSRCHandler
@@ -419,6 +418,7 @@ class NetjoinSession(threading.Thread):
             # TODO: CRITICAL - only start one announcer for one net_id
             self.receiver.announce(l2_type, net_id, beacon_interval)
 
+        logging.info("Announcing the newly joined network now")
         return True
 
     # Note: we forget the last message on disabling retransmission
