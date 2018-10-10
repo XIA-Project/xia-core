f8783303dd5cd8cffa9049c71b5890276c1bf80e
Nitin Gupta <nitingupta@cmu.edu>
2018-10-10 16:20:33 -0400

    ICID demo client was binding to incorrect address
    ICID demo client needs to bind to an address based on a temporary SID.
    This is the address to which the chunk will be pushed when available.

--- applications/chunk-demo/icid-demo-client.c
+++ applications/chunk-demo/icid-demo-client.c
@@ -1,3 +1,4 @@
+#include <Xkeys.h>
 #include <Xsocket.h>
 #include <dagaddr.hpp>
 #include <publisher/publisher.h>
@@ -15,7 +16,8 @@ void usage(const char *progname)
 int main(int argc, char **argv)
 {
 	int sockfd;
-	sockaddr_x sa;
+	sockaddr_x cidaddr;
+	char sid_string[XIA_XID_STR_SIZE];
 
 	if(argc != 2) {
 		usage(argv[0]);
@@ -24,24 +26,43 @@ int main(int argc, char **argv)
 
 	// Read in the address and convert to a form recognized by bind
 	Graph cid_dag(argv[1]);
-	cid_dag.fill_sockaddr(&sa);
+	cid_dag.fill_sockaddr(&cidaddr);
 
-	// Create a socket and bind to it
+	// Create a socket
 	if((sockfd = Xsocket(AF_XIA, SOCK_STREAM, 0)) < 0) {
 		std::cout << "Error creating socket" << std::endl;
 		return -1;
 	}
-	if(Xbind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+
+	// with a temporary SID
+	bzero(sid_string, sizeof(sid_string));
+	if(XmakeNewSID(sid_string, sizeof(sid_string))) {
+		std::cout << "Error making new SID" << std::endl;
+		return -1;
+	}
+
+	// used in a temporary address
+	struct addrinfo *ai;
+	if(Xgetaddrinfo(NULL, sid_string, NULL, &ai)) {
+		std::cout << "Error getting address" << std::endl;
+		return -1;
+	}
+
+	// Bind our server socket to the temporary address
+	sockaddr_x bind_addr;
+	memcpy(&bind_addr, (sockaddr_x *)ai->ai_addr, sizeof(sockaddr_x));
+	if(Xbind(sockfd, (struct sockaddr *)&bind_addr, sizeof(bind_addr)) < 0) {
 		std::cout << "Error binding to socket" << std::endl;
 		return -1;
 	}
+	// The socket now listens for incoming push requests
 	if(Xlisten(sockfd, 1)) {
 		std::cout << "Error listening to socket" << std::endl;
 		return -1;
 	}
 
 	// Call XinterestedInCID for given address
-	if(XinterestedInCID(sockfd, &sa)) {
+	if(XinterestedInCID(sockfd, &cidaddr)) {
 		std::cout << "Error submitting interest" << std::endl;
 		return -1;
 	}
@@ -65,6 +86,11 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	std::cout << "Passed: content retrieved" << std::endl;
+
+	Xclose(accepted_sock);
+	Xclose(sockfd);
+	Xfreeaddrinfo(ai);
+	XremoveSID(sid_string);
 
 	return 0;
 }
