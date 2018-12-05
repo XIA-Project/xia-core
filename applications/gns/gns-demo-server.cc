b7515256c6fe6c09b5f7191c90e3a51b9e36051b
Nitin Gupta <nitingupta@cmu.edu>
2018-12-05 15:34:35 -0500

    Cleanup keys and GNS entries

--- applications/gns/gns-demo-server.cc
+++ applications/gns/gns-demo-server.cc
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <atomic>
+
+#include <signal.h>
+#include <unistd.h>
 
 #include <cpr/cpr.h>
 
 #include "Xsocket.h"
-#include "Xkeys.h"
+#include "Xkeys.hh"
 #include "dagaddr.hpp"
+
+std::atomic<bool> stop(false);
+
+void sigint_handler(int)
+{
+	stop.store(true);
+}
 
 void work(int sockfd, std::string their_addr)
 {
@@ -15,10 +26,13 @@ void work(int sockfd, std::string their_addr)
 
 int main()
 {
-	char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
+	SIDKey sid;
 
-	//auto response = cpr::Get(cpr::Url{"http://172.16.252.131:24703/GNS/read?guid=70FD73C018FAEED5F041AACB9BA28BCB651A0F2B&field=clientaddr"});
-	//std::cout << response.text << std::endl;
+	struct sigaction action;
+	memset(&action, 0, sizeof(action));
+	action.sa_handler = sigint_handler;
+	sigfillset(&action.sa_mask);
+	sigaction(SIGINT, &action, NULL);
 
 	std::cout << "Creating a socket" << std::endl;
 	auto sockfd = Xsocket(AF_XIA, SOCK_STREAM, 0);
@@ -27,18 +41,12 @@ int main()
 		return -1;
 	}
 
-	std::cout << "Creating a new SID" << std::endl;
-	if(XmakeNewSID(sid_string, sizeof(sid_string))) {
-		std::cout << "ERROR creating new SID" << std::endl;
-		return -1;
-	}
-
 	std::cout << "Getting our address" << std::endl;
 	struct addrinfo hints, *ai;
 	bzero(&hints, sizeof(hints));
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_family = AF_XIA;
-	if(Xgetaddrinfo(NULL, sid_string, &hints, &ai) != 0) {
+	if(Xgetaddrinfo(NULL, sid.to_string().c_str(), &hints, &ai) != 0) {
 		std::cout << "Failed getting address" << std::endl;
 		// TODO: remove temporary SID
 		return -1;
@@ -58,7 +66,6 @@ int main()
 	Xlisten(sockfd, 5);
 
 	std::cout << "Registering our address with GNS" << std::endl;
-	//std::string gns_url("http:://localhost:5678/GNS/");
 	std::string gns_url("localhost:5678/GNS/");
 	std::string cmd = gns_url + "create?";
 	cmd += "guid=70FD73C018FAEED5F041AACB9BA28BCB651A0F2B";
@@ -71,7 +78,24 @@ int main()
 	std::cout << response.text << std::endl;
 
 	std::cout << "Waiting for connections" << std::endl;
+
+	// Set up for polling on sockfd to allow for graceful interruption
+	struct pollfd ufd;
+	ufd.fd = sockfd;
+	ufd.events = POLLIN;
+	unsigned nfds = 1;
+	int timeout = 500;	// milliseconds
+
 	while(true) {
+		int rc = Xpoll(&ufd, nfds, timeout);
+		if(rc <= 0) {
+			if(stop.load()) {
+				std::cout << "Interrupted. Cleaning up" << std::endl;
+				break;
+			}
+			continue;
+		}
+
 		int newsock;
 		sockaddr_x their_addr;
 		socklen_t sz = sizeof(their_addr);
@@ -83,6 +107,17 @@ int main()
 		auto t = std::thread(work, newsock, g.dag_string());
 		t.detach();
 	}
+
+	std::cout << "Unregistering our address from GNS" << std::endl;
+	cmd = gns_url + "removefield?";
+	cmd += "guid=70FD73C018FAEED5F041AACB9BA28BCB651A0F2B";
+	cmd += "&field=demoserveraddr";
+	std::cout << "Command: " << cmd << std::endl;
+
+	response = cpr::Get(cpr::Url(cmd));
+	std::cout << "Response status code: " << response.status_code << std::endl;
+	std::cout << response.text << std::endl;
+
 	Xclose(sockfd);
 	return 0;
 }
